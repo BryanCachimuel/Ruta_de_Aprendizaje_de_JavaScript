c6cf135662409ca5ec68b3672116344474e5f21a
BryanCachimuel <bryanloyo56@gmail.com>
2023-01-05 19:38:53 -0500

    agregando la funcionalidad para agregar tareas en el formulario y validando el campo input

--- Lista_Tareas/publico/app.js
+++ Lista_Tareas/publico/app.js
@@ -1,5 +1,7 @@
 /* Variables */
 const formulario = document.querySelector("#formulario");
+const tituloForm = document.querySelector("#titulo-formulario");
+let tareas = [];
 
 /* Eventos */
 (() => {
@@ -9,5 +11,24 @@ const formulario = document.querySelector("#formulario");
 /* Funciones */
 function validarFormulario(e){
     e.preventDefault();
-    alert('enviando datos');
+    // validar datos del input
+    const tarea = document.querySelector("#tarea").value;
+    if(!tarea.trim()){
+        tituloForm.textContent = "Formulario Vacio"
+        setTimeout(() => {
+            tituloForm.textContent = "Agregar Tareas"
+        }, 2000);
+        return
+    }
+
+    /* crear un objeto */
+    const objTarea = {
+        id: Date.now(),
+        tarea: tarea,
+        estado: false
+    }
+    
+    tareas = [...tareas, objTarea];
+    
+    
 }
