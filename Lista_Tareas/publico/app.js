866f9e81664490d7e47e952cb1248c0d0d567018
BryanCachimuel <bryanloyo56@gmail.com>
2023-01-05 20:03:27 -0500

    haciendo que se vea las tareas agregadas en una tarjeta

--- Lista_Tareas/publico/app.js
+++ Lista_Tareas/publico/app.js
@@ -1,6 +1,7 @@
 /* Variables */
 const formulario = document.querySelector("#formulario");
 const tituloForm = document.querySelector("#titulo-formulario");
+const task = document.querySelector(".tareas");
 let tareas = [];
 
 /* Eventos */
@@ -30,5 +31,33 @@ function validarFormulario(e){
     
     tareas = [...tareas, objTarea];
     
+   
+    formulario.reset(); // se resea el formulario y después de agregar una tarea está borra del input
     
+    mostrarHTML();  // va a poder permitir que las tareas se muestren al lado derecho del formulario
+}
+
+function mostrarHTML(){
+
+    // validando para que no se repita la tarea cuando se ingresa valores en el input
+    task.innerHTML = "";
+
+    if(tareas.length < 1){
+        const mensaje = document.createElement("h5");
+        mensaje.textContent = "~ SIN TAREAS ~";
+    }
+
+    tareas.forEach( (item) => {
+        const itemTarea = document.createElement("div");
+        itemTarea.classList.add("item-tarea");
+        itemTarea.innerHTML = `
+            <p>${item.tarea}</p>
+            <div class="botones">
+                <button data-id="${item.id}" class="eliminar">X</button>
+                <button data-id="${item.id}" class="completada">?</button>
+            </div>
+        `;
+        
+        task.appendChild(itemTarea)
+    })
 }
