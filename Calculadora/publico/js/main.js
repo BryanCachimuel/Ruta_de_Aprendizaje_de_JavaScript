13e594f60090e4929d2a510c6f339e08e848142c
BryanCachimuel <bryanloyo56@gmail.com>
2023-01-07 19:51:10 -0500

    creando una instancia de la clase Display para utilizar la función computar para hacer las operaciones con los botones de la calculadora

--- Calculadora/publico/js/main.js
+++ Calculadora/publico/js/main.js
@@ -4,6 +4,14 @@ const displayValorActual = document.getElementById('valor-actual');
 const botonesNumeros = document.querySelectorAll('.numero');
 const botonesOperadores = document.querySelectorAll('.operador');
 
-/* realizando una instancia de la clase Calculadora */
-const calculadora = new Calculadora();
-alert(calculadora.sumar(2,3))
+const display = new Display(displayValorAnterior, displayValorActual);
+
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () => {
+        display.agregarNumero(boton.innerHTML);
+    });
+});
+
+botonesOperadores.forEach(boton =>{
+    boton.addEventListener('click', () => display.computar(boton.value))
+})
