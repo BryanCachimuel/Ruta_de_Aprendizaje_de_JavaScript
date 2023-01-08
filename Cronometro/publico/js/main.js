9adbf9dfbe3e820610dd1eaf88f1df4dec49e7ca
BryanCachimuel <bryanloyo56@gmail.com>
2023-01-07 22:26:33 -0500

    creando la parte del funcionamiento con javascript para el ejercicio

---
+++ Cronometro/publico/js/main.js
@@ -0,0 +1,30 @@
+/* creando las variables para controlar los elementos del html */
+const cronometro_pantalla = document.getElementById('cronometro')
+const cronometro_opciones = document.getElementById('cronometro-opciones')
+const cronometro_border = document.getElementById('cronometro-border')
+const cronometro_score = document.getElementById('cronometro-score')
+
+let cseg = 0
+let seg = 0
+let contador = null
+let cantPoints = 0
+
+const conseguirContador = () => {
+
+}
+
+const iniciarCronometro = () => {
+
+}
+
+const pararCronometro = () => {
+
+}
+
+const resetearCronometro = () => {
+
+}
+
+const aniadirPunto = () => {
+
+}
