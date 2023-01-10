dc3bd23e27b1d146ac3932919c7c3df570c43d8d
BryanCachimuel <bryanloyo56@gmail.com>
2023-01-09 20:25:49 -0500

    creando las variables con las cuales se va a manipular el modo del juego

---
+++ Juego_Ahorcado/publico/js/main.js
@@ -0,0 +1,24 @@
+const wordContainer = document.getElementById('wordContainer');
+const startButton = document.getElementById('startButton');
+const usedLettersElement = document.getElementById('usedLetters');
+
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+ctx.canvas.width = 0;
+ctx.canvas.height = 0;
+
+/* valores para ir dibujando las partes del dibujo del ahorcado */
+const bodyParts = [
+    [4,2,1,1],
+    [4,3,1,2],
+    [3,5,1,1],
+    [5,5,1,1],
+    [3,3,1,1],
+    [5,3,1,1]
+];
+
+/* variables del juego */
+let selectedWord;
+let usedLetters;
+let mistakes;
+let hits;
