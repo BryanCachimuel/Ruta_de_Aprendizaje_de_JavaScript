c48b75361d957bcc9fefbdcb91f3daac6005962d
BryanCachimuel <bryanloyo56@gmail.com>
2023-01-14 19:58:49 -0500

    creando las funcionalidades para complementar la lógica del temporizador

--- Temporizador/publico/js/main.js
+++ Temporizador/publico/js/main.js
@@ -5,17 +5,35 @@ let contando = false
 function temporizar(){
 
     let temporizando = tiempo - contador
-    let contando = temporizando > 0
+    contando = temporizando > 0
 
     if(contador%2 == 0){
         select_id("frente").innerHTML = temporizando
+    }else{
+        select_id("atras").innerHTML = temporizando
     }
-    style("moneda").style.transform = `rotateY(${180*contador}deg)`
+    style("moneda").transform = `rotateY(${180*contador}deg)`
     if(contando){
         contador++
         setTimeout(() => {
             temporizar()
         }, 1000);
+    }
+}
+
+async function tomarTiempo(){
+    const {value: tiempo_input} = await swal.fire({
+        title:"Temporizador",
+        input:"number",
+        inputValue:10,
+        showCancelButton:true
+    })
+    if(!tiempo_input){
+        return
+    }
+    tiempo = tiempo_input
+    if(!contando){
+        temporizar()
     }
 }
 
