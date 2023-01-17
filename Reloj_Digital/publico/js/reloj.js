ba87ab75792561fac06f113042d8207b62e894e2
BryanCachimuel <bryanloyo56@gmail.com>
2023-01-16 22:53:34 -0500

    agregando la funcionalidad al ejercicio con javascript

---
+++ Reloj_Digital/publico/js/reloj.js
@@ -0,0 +1,17 @@
+(function(){
+    
+    let actualizarHora = function(){
+        let fecha = new Date();
+        let horas = fecha.getHours;
+        let ampm;
+        let minutos = fecha.getMinutes();
+        let segundos = fecha.getSeconds();
+        let diaSemana = fecha.getDay();
+        let dia = fecha.getDate();
+        let mes = fecha.getMonth();
+        let year = fecha.getFullYear();
+    };
+
+    actualizarHora();
+
+}())
