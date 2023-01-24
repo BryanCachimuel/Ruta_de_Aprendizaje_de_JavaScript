c2858593adb4751585b547b7cf970afd172ab1ec
BryanCachimuel <bryanloyo56@gmail.com>
2023-01-23 22:03:51 -0500

    creando el evento para que el carrito de compras pueda añadir o quitar ciertos eventos que seran invocados durante la resolución del problema

--- Ecomerce/publico/js/usuarios.js
+++ Ecomerce/publico/js/usuarios.js
@@ -1,82 +1,66 @@
-/* funciones para almacenar y traer los datos que se almacenan */
-function guardarAlmacenamientoLocal(llave, valor_a_guardar){
+// Funciones para almacenar y traer los datos que se almacenan
+function guardarAlmacenamientoLocal(llave, valor_a_guardar) {
     localStorage.setItem(llave, JSON.stringify(valor_a_guardar))
 }
-
-function obtenerAlmacenamientoLocal(llave){
+function obtenerAlmacenamientoLocal(llave) {
     const datos = JSON.parse(localStorage.getItem(llave))
     return datos
 }
-
 let productos = obtenerAlmacenamientoLocal('productos') || [];
 
-/* constantes las cuales se obtienes de las diferentes propiedades creadas en el html */
+// Variables que traemos de nuestro html
 const informacionCompra = document.getElementById('informacionCompra');
 const contenedorCompra = document.getElementById('contenedorCompra');
 const productosCompra = document.getElementById('productosCompra');
 const contenedor = document.getElementById('contenedor');
 const carrito = document.getElementById('carrito');
-const numero = document.getElementById('numero');
-const header = document.querySelector('#header');
+const numero = document.getElementById("numero");
+const header = document.querySelector("#header");
 const total = document.getElementById('total');
-const body = document.querySelector('body');
-const x = document.getElementById('x');
+const body = document.querySelector("body");
+const x = document.getElementById('x')
 
-/* variables que se van a utilizar en el proyecto */
+// Variables que vamos a usar en nuestoro proyecto
 let lista = []
-let valorTotal = 0
+let valortotal = 0
 
-/* scroll para la página web */
-window.addEventListener("scroll", function(){
-    if(contenedor.getBoundingClientRect().top < 10){
+// Scroll de nuestra pagina
+window.addEventListener("scroll", function () {
+    if (contenedor.getBoundingClientRect().top < 10) {
         header.classList.add("scroll")
-    }else{
+    }
+    else {
         header.classList.remove("scroll")
     }
 })
 
-window.addEventListener("load", () => {
+
+window.addEventListener('load', () => {
     visualizarProductos();
     contenedorCompra.classList.add("none")
 })
 
-function visualizarProductos(){
+function visualizarProductos() {
     contenedor.innerHTML = ""
-    for(let i = 0; i < productos.length; i++){
-        if(productos[i].existencia > 0){
-            contenedor.innerHTML += `<div>
-                                        <img src="${productos[i].urlImagen}">
-                                        <div class="informacion">
-                                            <p>${productos[i].nombre}</p>
-                                            <p class="precio">$${productos[i].valor}</p>
-                                            <button onclick=comprar(${i})>Comprar</button>
-                                        </div>
-                                    </div>`
-        }else{
-            contenedor.innerHTML += `<div>
-                                        <img src="${productos[i].urlImagen}">
-                                        <div class="informacion">
-                                            <p>${productos[i].nombre}</p>
-                                            <p class="precio">$${productos[i].valor}</p>
-                                            <p class="soldOut">Sold Out</p>
-                                        </div>
-                                     </div>`
+    for (let i = 0; i < productos.length; i++) {
+        if (productos[i].existencia > 0) {
+            contenedor.innerHTML += `<div><img src="${productos[i].urlImagen}"><div class="informacion"><p>${productos[i].nombre}</p><p class="precio">$${productos[i].valor}</p><button onclick=comprar(${i})>Comprar</button></div></div>`
+        }
+        else {
+            contenedor.innerHTML += `<div><img src="${productos[i].urlImagen}"><div class="informacion"><p>${productos[i].nombre}</p><p class="precio">$${productos[i].valor}</p><p class="soldOut">Sold Out</p></div></div>`
         }
     }
 }
 
-function comprar(indice){
-    lista.push({nombre: productos[indice].nombre,
-                precio: productos[indice].valor
-              })
-    
+function comprar(indice) {
+    lista.push({ nombre: productos[indice].nombre, precio: productos[indice].valor })
+
     let van = true
     let i = 0
-
-    while(van == true){
-        if(productos[i].nombre == productos[indice].nombre){
+    while (van == true) {
+        if (productos[i].nombre == productos[indice].nombre) {
             productos[i].existencia -= 1
-            if(productos.existencia == 0){
+            if (productos[i].existencia == 0) {
                 visualizarProductos()
             }
             van = false
@@ -88,3 +72,11 @@ function comprar(indice){
     numero.classList.add("diseñoNumero")
     return lista
 }
+
+carrito.addEventListener("click", function(){
+    body.style.overflow = "hidden"
+    contenedorCompra.classList.remove('none')
+    contenedorCompra.classList.add('contenedorCompra')
+    informacionCompra.classList.add('informacionCompra')
+    mostrarElemtrosLista()
+})
