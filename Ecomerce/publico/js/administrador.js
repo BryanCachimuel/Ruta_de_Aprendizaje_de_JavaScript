6be44f668a1e8359ad3c75236cb8935382643294
BryanCachimuel <bryanloyo56@gmail.com>
2023-01-24 19:33:24 -0500

    declarando las funciones para realizar un almacenamiento local y obtener los datos almacenados en el localstorage

---
+++ Ecomerce/publico/js/administrador.js
@@ -0,0 +1,11 @@
+function  guardarAlmacenamientoLocal(llave, valor_a_guardar){
+    localStorage.setItem(llave, JSON.stringify(valor_a_guardar))
+}
+
+function obtenerAlmacenamientoLocal(llave){
+    const datos = JSON.parse(localStorage.getItem(llave))
+    return datos
+}
+
+let productos = obtenerAlmacenamientoLocal('productos') || [];
+let mensaje = document.getElementById('mensaje')
