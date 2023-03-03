981058e5d742f67ea4a75d1a495aea0eab1eac38
BryanCachimuel <bryanloyo56@gmail.com>
2023-03-02 20:42:15 -0500

    terminando de realizar el funcionamiento js del ejercicio

--- LicuadoraJS/publico/js/main.js
+++ LicuadoraJS/publico/js/main.js
@@ -1,13 +1,30 @@
 let estadoLicuadora = "apagado";
-
 let licuadora = document.getElementById("blender");
+
+let sonidoLicuadora = document.getElementById("blender-sound");
+let botonLicuadora = document.getElementById("blender-button-sound");
 
 const controlarLicuadora = () => {
     if(estadoLicuadora == "apagada"){
         estadoLicuadora = "encendido";
-        console.log("me prendiste")
+        InstalandoSonidosLicuadora();
+        licuadora.classList.add("active");
+        console.log("me prendiste");
     }else{
         estadoLicuadora = "apagada";
-        console.log("me apagaste")
+        InstalandoSonidosLicuadora();
+        licuadora.classList.remove("active");
+        console.log("me apagaste");
+    }
+}
+
+const InstalandoSonidosLicuadora = () => {
+    if(sonidoLicuadora.paused){
+        botonLicuadora.play();
+        sonidoLicuadora.pause();
+        sonidoLicuadora.currentTime = 0;
+    }else{
+        botonLicuadora.play();
+        sonidoLicuadora.play();
     }
 }
