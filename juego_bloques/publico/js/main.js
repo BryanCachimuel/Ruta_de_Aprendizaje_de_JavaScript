e21536f4736e3482b0aded1efbe6ebceb7ff3cd1
BryanCachimuel <bryanloyo56@gmail.com>
2023-03-17 22:31:54 -0500

    creando las funciones addBloques y las función morverUsuario

--- juego_bloques/publico/js/main.js
+++ juego_bloques/publico/js/main.js
@@ -48,3 +48,46 @@ const bloques  = [
     new Bloque(340, 190),
     new Bloque(450, 190),
 ]
+
+//Funcion añadir bloques que
+function addBloques(){
+    for(let i = 0; i < bloques.length; i++){
+        const bloque = document.createElement('div')
+        bloque.classList.add('bloque')
+        bloque.style.left = bloques[i].bottomLeft[0] + 'px'
+        bloque.style.bottom = bloques[i].bottomLeft[1] + 'px'
+        contenedor.appendChild(bloque)   
+    }
+}
+//Añadir los bloques al juego
+addBloques()
+//Definir Usuario
+function dibujarUsuario(){
+    usuario.style.left = posicionActualUsuario[0] + 'px'
+    usuario.style.bottom = posicionActualUsuario[1] + 'px'
+}
+
+//Añadir Usuario
+const usuario = document.createElement('div')
+usuario.classList.add('usuario')
+contenedor.appendChild(usuario)
+dibujarUsuario()
+//Mover al usuario por el tablero
+function moverUsuario(e){
+    switch(e.key){
+        case 'ArrowLeft':
+            if(posicionActualUsuario[0] > 0){
+                posicionActualUsuario[0] -= 10
+                dibujarUsuario()
+            }
+            break
+        case 'ArrowRight':
+            if(posicionActualUsuario[0] < (anchoTablero - anchoBloque)){
+                posicionActualUsuario[0] += 10
+                dibujarUsuario()
+            }
+            break
+    }
+}
+//Añadir evento escuchador para el documento de
+document.addEventListener('keydown', moverUsuario)
