6699e91d6cfada063a2a464df17e41c62f07fba7
BryanCachimuel <bryanloyo56@gmail.com>
2023-03-17 22:30:30 -0500

    creando la clase Bloque y las constantes que definen cada bloque a utilizase

--- juego_bloques/publico/js/main.js
+++ juego_bloques/publico/js/main.js
@@ -15,3 +15,36 @@ let posicionActualBola = posicionInicialBola
 let xDireccionBola = 2
 let yDireccionBola = 2
 let diametro = 20
+
+//definir timer
+let timerID
+//Definicion de la clase bloque
+
+class Bloque{
+    constructor(ejeX, ejeY){
+        this.bottomLeft = [ejeX, ejeY]
+        this.bottomRigth = [ejeX + anchoBloque, ejeY]
+        this.topLeft = [ejeX, ejeY + altoBloque]
+        this.topRigth = [ejeX + anchoBloque, ejeY + altoBloque]
+
+    }
+}
+
+//Definir todos los bloques que
+const bloques  = [
+    new Bloque(10, 250),
+    new Bloque(120, 250),
+    new Bloque(230, 250),
+    new Bloque(340, 250),
+    new Bloque(450, 250),
+    new Bloque(10, 220),
+    new Bloque(120, 220),
+    new Bloque(230, 220),
+    new Bloque(340, 220),
+    new Bloque(450, 220),
+    new Bloque(10, 190),
+    new Bloque(120, 190),
+    new Bloque(230, 190),
+    new Bloque(340, 190),
+    new Bloque(450, 190),
+]
