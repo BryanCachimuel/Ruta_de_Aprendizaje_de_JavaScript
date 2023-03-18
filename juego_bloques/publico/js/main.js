e899fbdb3739a60c12d9a5a7dc09de94ed8e7a4d
BryanCachimuel <bryanloyo56@gmail.com>
2023-03-17 22:28:47 -0500

    creando las variables con las cuales va a funcionar el juego, parte 1

---
+++ juego_bloques/publico/js/main.js
@@ -0,0 +1,17 @@
+const contenedor = document.querySelector('.contenedor')
+//Definicion de medidas
+const altoTablero = 300
+const anchoTablero = 570
+const altoBloque = 20
+const anchoBloque = 100
+
+//definir posicion Usuario
+const posicionInicialUsuario = [230,10]
+let posicionActualUsuario = posicionInicialUsuario
+//Definir posicion de la bola
+const posicionInicialBola = [270,40]
+let posicionActualBola = posicionInicialBola
+//definicion particularidad de la bola
+let xDireccionBola = 2
+let yDireccionBola = 2
+let diametro = 20
