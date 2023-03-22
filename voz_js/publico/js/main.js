3da246f9ce71702290f985cd0dd0d358c93cce1c
BryanCachimuel <bryanloyo56@gmail.com>
2023-03-21 20:22:17 -0500

    creando el funcionamiento con javascript parte 2

--- voz_js/publico/js/main.js
+++ voz_js/publico/js/main.js
@@ -13,3 +13,24 @@ reconocimiento.onresult = event =>{
     contenido.innerHTML = mensaje
     leerTextoCondicionado(mensaje)
 }
+
+/* Función que se lanza para locutar lo grabado */
+const leerTextoSimple = (mensaje)=>{
+    const voz = new SpeechSynthesisUtterance()
+    voz.text = mensaje
+    window.speechSynthesis.speak(voz)
+}
+/* Función que condiciona la respuesta dependiendo de el contenido de la grabación */
+const leerTextoCondicionado = (mensaje)=>{
+    const voz = new SpeechSynthesisUtterance()
+    if(mensaje.includes('canal')){
+        voz.text = 'Tu canal me parece una porqueria'
+    }else{
+        voz.text = mensaje
+    }
+    window.speechSynthesis.speak(voz)
+}
+/* Evento para empezar a grabar pulsado el boton */
+btnGrabarTexto.addEventListener('click', ()=>{
+    reconocimiento.start()
+})
