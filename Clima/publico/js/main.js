fc49e8a6001d5a4d73ef77705563fa3ecf810009
BryanCachimuel <bryanloyo56@gmail.com>
2023-04-02 20:36:58 -0500

    realizando la función del llamado al API que da información sobre el clima de ciudades y paises

--- Clima/publico/js/main.js
+++ Clima/publico/js/main.js
@@ -13,3 +13,24 @@ form.addEventListener('submit', (e) => {
 
     callAPI(nameCity.value, nameCountry.value);
 })
+
+function callAPI(city, country){
+    const apiId = '41d1d7f5c2475b3a16167b30bc4f265c';
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiId}`;
+
+    fetch(url)
+        .then(data => {
+            return data.json();
+        })
+        .then(dataJSON => {
+            if (dataJSON.cod === '404') {
+                showError('Ciudad no encontrada...');
+            } else {
+                clearHTML();
+                showWeather(dataJSON);
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
