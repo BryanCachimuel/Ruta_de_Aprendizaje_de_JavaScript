5f949fbc03541fc4e940e1472b41e68258de6ad5
BryanCachimuel <bryanloyo56@gmail.com>
2023-04-21 20:38:25 -0500

    creando los métodos init, startGame, updateRound y createSequence

--- Juego_Simon_Dice/publico/js/main.js
+++ Juego_Simon_Dice/publico/js/main.js
@@ -23,4 +23,29 @@ class Simon {
             new Audio('./publico/sounds/4.mp3'),
         ]
     }
+
+    init(){
+        this.display.startButton.onclick = () => this.startGame();
+    }
+
+    startGame(){
+        this.display.startButton.disabled = true;
+        this.updateRound(0);
+        this.userPosition = 0;
+        this.sequence = this.createSequence();
+        this.buttons.forEach((element, i) => {
+            element.classList.remove('winner');
+            element.onclick = () => this.buttonClick(i);
+        });
+        this.showSequence();
+    }
+
+    updateRound(value){
+        this.roundd = value;
+        this.display.round.textContent = `Round ${this.round}`;
+    }
+
+    createSequence(){
+        return Array.from({length: this.totalRounds}, () => this.getRandomColor());
+    }
 }
