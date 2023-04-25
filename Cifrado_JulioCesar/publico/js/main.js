1987788dad124cd35ecca1d7432901b55ddc1a3a
BryanCachimuel <bryanloyo56@gmail.com>
2023-04-24 20:31:58 -0500

    desarrollando el método printChar

--- Cifrado_JulioCesar/publico/js/main.js
+++ Cifrado_JulioCesar/publico/js/main.js
@@ -8,3 +8,16 @@ const shifMessage = () => {
     const wordArray = [...inputOriginal.value.toUpperCase()];
     printChar(0, wordArray);
 }
+
+const printChar = (currentLetterIndex, wordArray) => {
+    if(wordArray.length === currentLetterIndex) return;
+    inputOriginal.value = inputOriginal.value.substring(1)
+    const spanChar = document.createElement("span");
+    resultado.appendChild(spanChar);
+    animateChar(spanChar)
+        .then( () => {
+            const charSinCodificar = wordArray[currentLetterIndex];
+            spanChar.innerHTML = alfabeto.includes(charSinCodificar) ? alfabeto[(alfabeto.indexOf(charSinCodificar) + parseInt(rango.value)) % alfabeto.length] : charSinCodificar
+            printChar(currentLetterIndex + 1, wordArray)
+        });
+}
