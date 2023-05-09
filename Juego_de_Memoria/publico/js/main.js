6838d64b675e7ec935045b0fcca9139068f78863
BryanCachimuel <bryanloyo56@gmail.com>
2023-05-08 19:33:47 -0500

    creando la función setNewGame y creando un evento para activar el funcionamiento de la clase Memorama

--- Juego_de_Memoria/publico/js/main.js
+++ Juego_de_Memoria/publico/js/main.js
@@ -104,4 +104,13 @@ class Memorama {
         }
     }
 
+    setNewGame() {
+        this.removeClickEvents();
+        this.cards.forEach(card => card.classList.remove("opened"));
+        setTimeout(this.startGame.bind(this), 1000);
+    }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    new Memorama();
+});
