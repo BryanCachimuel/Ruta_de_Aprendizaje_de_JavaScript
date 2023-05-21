5207ecad75ec4391a291812fbe8378c02b68674b
BryanCachimuel <bryanloyo56@gmail.com>
2023-05-20 22:19:48 -0500

    creando la función revisarSiHayGanador dentro de los recuadros seleccionados por los usuarios

--- Juego_Triqui_Tracas/publico/js/main.js
+++ Juego_Triqui_Tracas/publico/js/main.js
@@ -25,3 +25,36 @@ cuadrados.forEach((cuadrado, posicion) => {
         }
     })
 })
+
+modal.querySelector("button").addEventListener("click", () => {
+    cuadrados.forEach(cuadrado => {
+        cuadrado.textContent = "";
+        cuadrado.classList.toggle("ganador", false);
+    })
+    modal.close();
+    estadoJuego = "P1";
+})
+
+function revisarSiHayGanador(){
+    const tablero = Array.from(cuadrados).map(cuadrado => cuadrado.textContent);
+    console.log(tablero)
+    // Reviso filas
+    for (let i = 0; i < 9; i+=3) {
+        if(tablero[i] && tablero[i] === tablero[i+1] && tablero[i] === tablero[i+2]){
+            return ([i,i+1,i+2]);
+        }
+    }
+    // Reviso columnas
+    for (let i = 0; i < 3; i++) {
+        if(tablero[i] && tablero[i] === tablero[i+3] && tablero[i] === tablero[i+6]){
+            return ([i,i+3,i+6]);
+        }
+    }
+    // Reviso oblicuas
+    if(tablero[0] && tablero[0] === tablero[4] && tablero[0] === tablero[8]) return [0,4,8];
+    if(tablero[2] && tablero[2] === tablero[4] && tablero[2] === tablero[6]) return [2,4,6];
+
+    //Reviso empate
+    if(tablero.includes("")) return false;
+    return "empate";
+}
