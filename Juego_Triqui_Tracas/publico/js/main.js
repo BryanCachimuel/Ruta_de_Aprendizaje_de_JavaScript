71cc0711a8e8d3c9211ef3c208f2de7fe0f5de3f
BryanCachimuel <bryanloyo56@gmail.com>
2023-05-21 20:12:49 -0500

    correcciones de los métodos para el funcionamiento adecuado del movimiento de las figuras dentro de las posiciones del juego

--- Juego_Triqui_Tracas/publico/js/main.js
+++ Juego_Triqui_Tracas/publico/js/main.js
@@ -1,36 +1,36 @@
-/* creando los caracteres del tablero */
-const x = "X";
-const o = "O";
+//Caracteres para el tablero
+const x = "✖";
+const o = "〇";
 
-/* elementos de la página */
+//Elementos de la página
 const cuadrados = document.querySelectorAll(".cuadrado");
 const modal = document.querySelector("dialog");
 const textoModal = modal.querySelector("h2");
-let estadoJuego = "P1";
+let estadoJuego = "P1" // P1 | P2 | PAUSA
 
-cuadrados.forEach((cuadrado, posicion) => {
-    cuadrados.addEventListener("click", () => {
+cuadrados.forEach((cuadrado, posicion) =>{
+    cuadrado.addEventListener("click", ()=>{
         if(estadoJuego === "PAUSA") return;
         if(cuadrado.textContent !== "") return;
         cuadrado.textContent = estadoJuego === "P1" ? x : o;
         estadoJuego = estadoJuego === "P1" ? "P2" : "P1";
         const posicionGanadora = revisarSiHayGanador();
-        console.log(typeof posicionGanadora);
-        if(typeof posicionGanadora === "object"){
+        console.log(typeof posicionGanadora)
+        if(typeof posicionGanadora === "object") {
             ganar(posicionGanadora)
             return
         };
-        if(posicionGanadora === "empate"){
+        if (posicionGanadora === "empate"){
             mostrarModal("Empate")
         }
     })
 })
 
-modal.querySelector("button").addEventListener("click", () => {
+modal.querySelector("button").addEventListener("click", ()=>{
     cuadrados.forEach(cuadrado => {
         cuadrado.textContent = "";
-        cuadrado.classList.toggle("ganador", false);
-    })
+        cuadrado.classList.toggle("ganador",false);
+    });
     modal.close();
     estadoJuego = "P1";
 })
@@ -38,18 +38,21 @@ modal.querySelector("button").addEventListener("click", () => {
 function revisarSiHayGanador(){
     const tablero = Array.from(cuadrados).map(cuadrado => cuadrado.textContent);
     console.log(tablero)
+
     // Reviso filas
     for (let i = 0; i < 9; i+=3) {
         if(tablero[i] && tablero[i] === tablero[i+1] && tablero[i] === tablero[i+2]){
             return ([i,i+1,i+2]);
         }
     }
+
     // Reviso columnas
     for (let i = 0; i < 3; i++) {
         if(tablero[i] && tablero[i] === tablero[i+3] && tablero[i] === tablero[i+6]){
             return ([i,i+3,i+6]);
         }
     }
+
     // Reviso oblicuas
     if(tablero[0] && tablero[0] === tablero[4] && tablero[0] === tablero[8]) return [0,4,8];
     if(tablero[2] && tablero[2] === tablero[4] && tablero[2] === tablero[6]) return [2,4,6];
