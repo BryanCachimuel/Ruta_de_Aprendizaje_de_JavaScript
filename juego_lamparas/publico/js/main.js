de98bdba7b54b176dcae942a60a670f01a2f28fd
BryanCachimuel <bryanloyo56@gmail.com>
2023-06-16 21:28:44 -0500

    declarando las variables y obtieniendo mediante eventos js los id y clases de las eiquetas del index.html

---
+++ juego_lamparas/publico/js/main.js
@@ -0,0 +1,4 @@
+let encendidas = [false, false, false];
+let luces = document.querySelectorAll(".luz");
+let textos = document.querySelectorAll(".texto");
+let inputs = document.querySelectorAll("input");
