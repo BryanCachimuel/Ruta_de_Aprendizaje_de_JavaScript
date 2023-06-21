77675e37faaef817a5fff7b827b5fcb3b3d610c2
BryanCachimuel <bryanloyo56@gmail.com>
2023-06-20 19:01:37 -0500

    agregando las funciones para comenzar el juego y cargar la bandera dentro del juego

--- adivina_la_bandera/publico/js/main.js
+++ adivina_la_bandera/publico/js/main.js
@@ -1,5 +1,5 @@
 let banderas = ["pa.svg", "bo.svg", "ad.svg", "gb.svg", "na.svg"];
-let correcta = [2,2,1,1,0];
+let correcta = [2, 2, 1, 1, 0];
 let opciones = [];
 
 //cargo en el arreglo opciones las opciones a mostrar en cada jugada
@@ -14,3 +14,28 @@ let posActual = 0;
 
 //variable que guarda la cantidad acertadas hasta el moemento
 let cantidadAcertadas = 0;
+
+// función para dar inicio al juego
+function comenzarJuego() {
+  //reseteamos las variables
+  posActual = 0;
+  cantidadAcertadas = 0;
+  //activamos las pantallas necesarias
+  document.getElementById("pantalla-inicial").style.display = "none";
+  document.getElementById("pantalla-juego").style.display = "block";
+  cargarBandera();
+}
+
+//funcion que carga la siguiente bandera y sus opciones
+function cargarBandera() {
+  if (banderas.length <= posActual) {
+    terminarJuego();
+  } else {
+    limpiarOpciones();
+
+    document.getElementById("imgBandera").src = "img/" + banderas[posActual];
+    document.getElementById("n0").innerHTML = opciones[posActual][0];
+    document.getElementById("n1").innerHTML = opciones[posActual][1];
+    document.getElementById("n2").innerHTML = opciones[posActual][2];
+  }
+}
