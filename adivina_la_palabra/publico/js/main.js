13f7b9fb5964d90f46e9cd80abc67319e379629b
BryanCachimuel <bryanloyo56@gmail.com>
2023-06-23 19:43:45 -0500

    determinando los arreglos que contendrán los nombres claves conlos cuales se va a realizar el juego y las variables principales

---
+++ adivina_la_palabra/publico/js/main.js
@@ -0,0 +1,20 @@
+//Arreglo que contiene las palabras para jugar
+let arrayPalabras =["GUITARRA", "ELEFANTE", "TURQUESA", "MARIELA", "TECLADO", "INGLATERRA"];
+
+//Arreglo que contiene las ayudas de cada palabra
+let ayudas = [
+    "Instrumento Musical",
+    "Animal de la selva",
+    "Es un color",
+    "Nombre de mujer",
+    "Hardware de computadora",
+    "Es un Pais"
+]
+
+let cantPalabrasJugadas = 0;
+let intentosRestantes = 5;
+let posActual;
+let arrayPalabraActual = [];
+let cantidadAcertadas = 0;
+let divsPalabraActual = [];
+let totalQueDebeAcertar;
