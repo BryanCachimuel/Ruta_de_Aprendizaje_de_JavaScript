4863561fbe813642ad900baae9443d33a491446c
BryanCachimuel <bryanloyo56@gmail.com>
2023-06-25 19:31:45 -0500

    crear el script para realizar un circulo en las cuales se forme el abecedario de la A a la Z

--- Juego_de_Pasapalabras/publico/js/main.js
+++ Juego_de_Pasapalabras/publico/js/main.js
@@ -64,7 +64,7 @@ const bd_juego = [
 ];
 
 //preguntas que ya han sido contestadas. Si estan en 0 no han sido contestadas
-var estadoPreguntas = [0,0,0,0,0,0,0,0,0,0]
+var estadoPreguntas = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 var cantidadAcertadas = 0;
 
 //variable que mantiene el número de pregunta actual
@@ -79,9 +79,25 @@ var countdown;
 
 //boton comenzar
 var comenzar = document.getElementById("comenzar");
-comenzar.addEventListener("click", function(event) {
+comenzar.addEventListener("click", function (event) {
   document.getElementById("pantalla-inicial").style.display = "none";
   document.getElementById("pantalla-juego").style.display = "block";
   largarTiempo();
   cargarPregunta();
 });
+
+//Creamos el circúlo con las letras de la A a la Z
+const container = document.querySelector(".container");
+for (let i = 1; i <= TOTAL_PREGUNTAS; i++) {
+  const circle = document.createElement("div");
+  circle.classList.add("circle");
+  circle.textContent = String.fromCharCode(i + 96);
+  circle.id = String.fromCharCode(i + 96).toUpperCase();
+  container.appendChild(circle);
+
+  const angle = ((i - 1) / TOTAL_PREGUNTAS) * Math.PI * 2 - Math.PI / 2;
+  const x = Math.round(95 + 120 * Math.cos(angle));
+  const y = Math.round(95 + 120 * Math.sin(angle));
+  circle.style.left = `${x}px`;
+  circle.style.top = `${y}px`;
+}
