0fbe71c398a7a4bff2572d5ea6453b01809e872b
BryanCachimuel <bryanloyo56@gmail.com>
2023-06-29 19:00:03 -0500

    creando las funciones agregarOpciones agregarEliminarAspan

--- completa_texto/publico/js/main.js
+++ completa_texto/publico/js/main.js
@@ -11,3 +11,25 @@ let txtResultado = document.getElementById("resultado");
 
 //Arreglo que contiene el orden de las palabras que el usuario va eligiendo. Tambien me sirve para saber las posiciones disponibles
 let posDisponible = ["", "", "", "", "", "", ""];
+
+//Función que agrega las opciones 
+function agregarOpciones(){
+    palabras_juego.forEach(element => {
+        let div = document.createElement("div");
+        div.className = "palabra";
+        div.innerHTML = element;
+        div.setAttribute("onclick", "completar(this)");
+        contenedorOpciones.appendChild(div);
+    });
+}
+agregarOpciones();
+
+//agrego el método remove onclick a cada elemento span del texto
+function agregarEliminarAspan(){
+    var spans = document.getElementsByTagName("span");
+    for(let i=0; i < spans.length; i++){
+        spans[i].setAttribute("onclick", "remove(this)");
+    }
+}
+agregarEliminarAspan();
+
