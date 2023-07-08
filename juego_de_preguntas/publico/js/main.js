ea13d8170796caad8c2eb270791b15a1d9d48480
BryanCachimuel <bryanloyo56@gmail.com>
2023-07-07 22:10:17 -0500

    creando las preguntas con las cuales se va a poder realizar las preguntas para el juego

---
+++ juego_de_preguntas/publico/js/main.js
@@ -0,0 +1,51 @@
+//base de datos local de preguntas
+const bd_juego = [
+    {
+        id:0,
+        pregunta:"¿Cuál es el país más pequeño del mundo?",
+        op0:"Estado Vaticano",
+        op1:"Mónaco",
+        op2:"San Marino",
+        correcta:"0"
+    },
+    {
+        id:1,
+        pregunta:"¿Cuántos océanos hay en la Tierra?",
+        op0:"Seis",
+        op1:"Cinco",
+        op2:"Cuatro",
+        correcta:"1"
+    },
+    {
+        id:2,
+        pregunta:"¿Qué país tiene más habitantes?",
+        op0:"China",
+        op1:"Estados Unidos",
+        op2:"Rusia",
+        correcta:"0"
+    },
+    {
+        id:3,
+        pregunta:"¿Qué país es el más grande del mundo?",
+        op0:"Rusia",
+        op1:"Estados Unidos",
+        op2:"India",
+        correcta:"0"
+    },
+    {
+        id:4,
+        pregunta:"¿Cuál es la montaña más alta del mundo?",
+        op0:"Acongagua",
+        op1:"Tabor",
+        op2:"Everest",
+        correcta:"2"
+    },
+    {
+        id:5,
+        pregunta:"¿Cuál es el río más largo del mundo?",
+        op0:"Nilo",
+        op1:"Amazonas",
+        op2:"Eufrates",
+        correcta:"0"
+    },
+]
