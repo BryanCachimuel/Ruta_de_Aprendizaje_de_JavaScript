a57c5bcb5cac0f90677994133ae347136a5ca76e
BryanCachimuel <bryanloyo56@gmail.com>
2023-07-15 22:35:59 -0500

    determinando las variables principales con las cuales se va a controlar desde js los días, horas, minutos y segundos

---
+++ ejercicio_cuenta_regresiva/publico/js/app.js
@@ -0,0 +1,8 @@
+const dias = document.getElementById("days");
+const horas = document.getElementById("hours");
+const minutos = document.getElementById("min");
+const segundos = document.getElementById("sec");
+
+const navidad = new Date("December 25, 2023 00:00:00");
+
+
