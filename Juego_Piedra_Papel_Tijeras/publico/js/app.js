bce73f98bd31944836c9fc0dcb784384035650ea
BryanCachimuel <bryanloyo56@gmail.com>
2023-07-17 21:47:41 -0500

    creando las constantes que hacen enfacis a las etiquetas dadas en la maquetación

---
+++ Juego_Piedra_Papel_Tijeras/publico/js/app.js
@@ -0,0 +1,12 @@
+const seccionBatalla = document.getElementById("campo-batalla");
+const msjBatalla = document.getElementById("msj-batalla");
+const imgAtaqueJugador = document.getElementById("img-ataque-jugador");
+const imgAtaquePC = document.getElementById("img-ataque-pc");
+const btnPiedra = document.getElementById("btn-piedra");
+const btnPapel = document.getElementById("btn-papel");
+const btnTijeras = document.getElementById("btn-tijeras");
+
+let opcionJugador;
+let opcionPc;
+let imgJugador;
+let imgPc;
