2c146bca57b0234267a381fec448c154154b9c8e
BryanCachimuel <bryanloyo56@gmail.com>
2023-07-20 19:45:40 -0500

    creando un arreglo el cual contendrá la ubicación de las imagenes correspondientes a las letras del abecedario

---
+++ Lenguaje_de_Senias/publico/js/app.js
@@ -0,0 +1,111 @@
+//arraglo que contiene la ubicación de las imagenes
+const array = [
+    {
+        'case': ' ',
+        'imagen': './img/ESPACIO.PNG'
+    },
+    {
+        'case': 'A',
+        'imagen': './img/A.PNG'
+    },
+    {
+        'case': 'B',
+        'imagen': './img/B.PNG'
+    },
+    {
+        'case': 'C',
+        'imagen': './img/C.PNG'
+    },
+    {
+        'case': 'D',
+        'imagen': './img/D.PNG'
+    },
+    {
+        'case': 'E',
+        'imagen': './img/E.PNG'
+    },
+    {
+        'case': 'F',
+        'imagen': './img/F.PNG'
+    },
+    {
+        'case': 'G',
+        'imagen': './img/G.PNG'
+    },
+    {
+        'case': 'H',
+        'imagen': './img/H.PNG'
+    },
+    {
+        'case': 'I',
+        'imagen': './img/I.PNG'
+    },
+    {
+        'case': 'J',
+        'imagen': './img/J.PNG'
+    },
+    {
+        'case': 'K',
+        'imagen': './img/K.PNG'
+    },
+    {
+        'case': 'L',
+        'imagen': './img/L.PNG'
+    },
+    {
+        'case': 'M',
+        'imagen': './img/M.PNG'
+    },
+    {
+        'case': 'N',
+        'imagen': './img/N.PNG'
+    },
+    {
+        'case': 'O',
+        'imagen': './img/O.PNG'
+    },
+    {
+        'case': 'P',
+        'imagen': './img/P.PNG'
+    },
+    {
+        'case': 'Q',
+        'imagen': './img/Q.PNG'
+    },
+    {
+        'case': 'R',
+        'imagen': './img/R.PNG'
+    },
+    {
+        'case': 'S',
+        'imagen': './img/S.PNG'
+    },
+    {
+        'case': 'T',
+        'imagen': './img/T.PNG'
+    },
+    {
+        'case': 'U',
+        'imagen': './img/U.PNG'
+    },
+    {
+        'case': 'V',
+        'imagen': './img/V.PNG'
+    },
+    {
+        'case': 'W',
+        'imagen': './img/W.PNG'
+    },
+    {
+        'case': 'X',
+        'imagen': './img/X.PNG'
+    },
+    {
+        'case': 'Y',
+        'imagen': './img/Y.PNG'
+    },
+    {
+        'case': 'Z',
+        'imagen': './img/Z.PNG'
+    }
+];
