1efde964f766d7047717d9be3a0d4f729d641cbf
BryanCachimuel <bryanloyo56@gmail.com>
2023-07-21 20:16:57 -0500

    correcciones de errores al llamado de las imagenes por cada letra que representa en el abecedario

--- Lenguaje_de_Senias/publico/js/app.js
+++ Lenguaje_de_Senias/publico/js/app.js
@@ -2,111 +2,111 @@
 const array = [
     {
         'case': ' ',
-        'imagen': './img/ESPACIO.PNG'
+        'imagen': '../img/ESPACIO.PNG'
     },
     {
         'case': 'A',
-        'imagen': './img/A.PNG'
+        'imagen': '../img/A.PNG'
     },
     {
         'case': 'B',
-        'imagen': './img/B.PNG'
+        'imagen': '../img/B.PNG'
     },
     {
         'case': 'C',
-        'imagen': './img/C.PNG'
+        'imagen': '../img/C.PNG'
     },
     {
         'case': 'D',
-        'imagen': './img/D.PNG'
+        'imagen': '../img/D.PNG'
     },
     {
         'case': 'E',
-        'imagen': './img/E.PNG'
+        'imagen': '../img/E.PNG'
     },
     {
         'case': 'F',
-        'imagen': './img/F.PNG'
+        'imagen': '../img/F.PNG'
     },
     {
         'case': 'G',
-        'imagen': './img/G.PNG'
+        'imagen': '../img/G.PNG'
     },
     {
         'case': 'H',
-        'imagen': './img/H.PNG'
+        'imagen': '../img/H.PNG'
     },
     {
         'case': 'I',
-        'imagen': './img/I.PNG'
+        'imagen': '../img/I.PNG'
     },
     {
         'case': 'J',
-        'imagen': './img/J.PNG'
+        'imagen': '../img/J.PNG'
     },
     {
         'case': 'K',
-        'imagen': './img/K.PNG'
+        'imagen': '../img/K.PNG'
     },
     {
         'case': 'L',
-        'imagen': './img/L.PNG'
+        'imagen': '../img/L.PNG'
     },
     {
         'case': 'M',
-        'imagen': './img/M.PNG'
+        'imagen': '../img/M.PNG'
     },
     {
         'case': 'N',
-        'imagen': './img/N.PNG'
+        'imagen': '../img/N.PNG'
     },
     {
         'case': 'O',
-        'imagen': './img/O.PNG'
+        'imagen': '../img/O.PNG'
     },
     {
         'case': 'P',
-        'imagen': './img/P.PNG'
+        'imagen': '../img/P.PNG'
     },
     {
         'case': 'Q',
-        'imagen': './img/Q.PNG'
+        'imagen': '../img/Q.PNG'
     },
     {
         'case': 'R',
-        'imagen': './img/R.PNG'
+        'imagen': '../img/R.PNG'
     },
     {
         'case': 'S',
-        'imagen': './img/S.PNG'
+        'imagen': '../img/S.PNG'
     },
     {
         'case': 'T',
-        'imagen': './img/T.PNG'
+        'imagen': '../img/T.PNG'
     },
     {
         'case': 'U',
-        'imagen': './img/U.PNG'
+        'imagen': '../img/U.PNG'
     },
     {
         'case': 'V',
-        'imagen': './img/V.PNG'
+        'imagen': '../img/V.PNG'
     },
     {
         'case': 'W',
-        'imagen': './img/W.PNG'
+        'imagen': '../img/W.PNG'
     },
     {
         'case': 'X',
-        'imagen': './img/X.PNG'
+        'imagen': '../img/X.PNG'
     },
     {
         'case': 'Y',
-        'imagen': './img/Y.PNG'
+        'imagen': '../img/Y.PNG'
     },
     {
         'case': 'Z',
-        'imagen': './img/Z.PNG'
+        'imagen': '../img/Z.PNG'
     }
 ];
 
@@ -145,9 +145,8 @@ function traduccion(){
                 if(descomposicion.toUpperCase() == array[j].case){
                     //se crea la etiqueta de la imagen correspondiente
                     opcionImagen =  `
-                        <img class="remove" src="${array[j].imagen}" alt="${array[j].name}">
+                        <img class="remove" src="./img/${array[j].imagen}">
                     `;
-
                     contenedorTraslate.innerHTML += opcionImagen;
                     btnTraducirName.disabled = true;
                     InputAccion.disabled = true;
