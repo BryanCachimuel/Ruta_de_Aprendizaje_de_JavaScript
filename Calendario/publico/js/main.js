4a60acbc002c7631cbee7ced66a19fb4dd5d727a
BryanCachimuel <bryanloyo56@gmail.com>
2023-08-11 19:24:00 -0500

    creando las funciones getTotalDays, isLeap y startDay

--- Calendario/publico/js/main.js
+++ Calendario/publico/js/main.js
@@ -29,4 +29,25 @@ const writeMonth = (month) => {
       dates.innerHTML += ` <div class="calendar__date calendar__item">${i}</div>`;
     }
   }
-};
+}
+
+const getTotalDays = month => {
+  if(month === -1) month = 11;
+  if (month == 0 || month == 2 || month == 4 || month == 6 || month == 7 || month == 9 || month == 11) {
+      return  31;
+  } else if (month == 3 || month == 5 || month == 8 || month == 10) {
+      return 30;
+  } else {
+      return isLeap() ? 29:28;
+  }
+}
+
+const isLeap = () => {
+  return ((currentYear % 100 !==0) && (currentYear % 4 === 0) || (currentYear % 400 === 0));
+}
+
+const startDay = () => {
+  let start = new Date(currentYear, monthNumber, 1);
+  return ((start.getDay()-1) === -1) ? 6 : start.getDay()-1;
+}
+
