be3bf893e07e5d5501d9f773fc5951f9ad07d31a
BryanCachimuel <bryanloyo56@gmail.com>
2023-09-06 21:56:54 -0500

    creando una función llamada bar donde se determina la resolución del proceso del dibujado en el progress-bar-circular de acuerdo al número ingresado dentro del input del ejercicio

--- Progress-Bar-Circular/publico/js/main.js
+++ Progress-Bar-Circular/publico/js/main.js
@@ -6,4 +6,26 @@ let start = 0;
 
 btn.addEventListener("click", bar);
 
-
+function bar() {
+    let progress = setInterval(() => {
+      if (start < inp.value) {
+        start++;
+        progressEND();
+      } else {
+        start--;
+        progressEND();
+      }
+  
+      function progressEND() {
+        value.textContent = `${start}%`;
+        progressCircular.style.background = `conic-gradient(#00a6ff ${
+          start * 3.6
+        }deg, #ededed 0deg)`;
+        //console.log(start);
+        if (start == inp.value) {
+          clearInterval(progress);
+          inp.value = "";
+        }
+      }
+    }, 50);
+  }
