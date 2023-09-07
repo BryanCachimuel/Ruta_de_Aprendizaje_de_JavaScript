774061a661a7a12f5fd2098fddd695ee051f6473
BryanCachimuel <bryanloyo56@gmail.com>
2023-09-06 21:54:29 -0500

    obteniendo el contenido de las etiquetas del maquetado html con eventos querySelector de javascript

---
+++ Progress-Bar-Circular/publico/js/main.js
@@ -0,0 +1,9 @@
+let progressCircular = document.querySelector(".progress-circular");
+let btn = document.querySelector("button");
+let inp = document.querySelector("input");
+let value = document.querySelector(".value");
+let start = 0;
+
+btn.addEventListener("click", bar);
+
+
