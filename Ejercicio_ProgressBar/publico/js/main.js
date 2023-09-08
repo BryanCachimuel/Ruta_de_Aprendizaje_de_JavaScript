5a6ba7c8837674c31502b42833666d29947b21ee
BryanCachimuel <bryanloyo56@gmail.com>
2023-09-07 22:28:08 -0500

    creando la función changeWidth en donde se calculan los valores que se tomen en los inputs y se los dibuje en el progress-bar adecuadamente

--- Ejercicio_ProgressBar/publico/js/main.js
+++ Ejercicio_ProgressBar/publico/js/main.js
@@ -4,6 +4,10 @@ const maxInput = document.querySelector(".maxInput");
 let finalValue = 0;
 let max = 0;
 
+function changeWidth(){
+    progress.style.width = `${(finalValue / max)* 100}%`;
+    progress.innerText = `${Math.ceil((finalValue / max) * 100)}%`;
+}
 
 input.addEventListener("keyup", function(){
     finalValue = parseInt(input.value, 10);
