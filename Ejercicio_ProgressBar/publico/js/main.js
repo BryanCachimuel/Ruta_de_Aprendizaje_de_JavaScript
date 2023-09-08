fbc6e764462b26da51c8c7952e4d9ddc17af87b8
BryanCachimuel <bryanloyo56@gmail.com>
2023-09-07 22:23:59 -0500

    obteniendo mediante el evento querySelector los valores de las etiquetas del maquetado

--- Ejercicio_ProgressBar/publico/js/main.js
+++ Ejercicio_ProgressBar/publico/js/main.js
@@ -1,31 +1,9 @@
-class ProgressBar {
-    constructor(elemento, valorInicial = 0){
-        this.valueElem = elemento.querySelector('.progress-bar-value');
-        this.fillElem = elemento.querySelector('.progress-bar-fill');
+const progress = document.querySelector(".progress-done");
+const input = document.querySelector(".input");
+const maxInput = document.querySelector(".maxInput");
+let finalValue = 0;
+let max = 0;
 
-        this.setValue(valorInicial);
-    }
 
-    setValue(nuevoValor){
-        if(nuevoValor < 0){
-            nuevoValor = 0;
-        }
 
-        if(nuevoValor > 100){
-            nuevoValor = 100;
-        }
-
-        this.value = nuevoValor;
-        this.update();
-    }
-
-    update(){
-        const porcentaje = this.value + '%';
-
-        this.fillElem.style.width = porcentaje;
-        this.valueElem.textContent = porcentaje;
-    }
-}
-
-const valorPorcentaje = new ProgressBar(document.querySelector('.progress-bar'), 40);
 
