657cf6d000a4bfab99598401e1cee353d3797d64
BryanCachimuel <bryanloyo56@gmail.com>
2023-09-13 21:20:05 -0500

    creando un setIntwerval para controlar el tiempo para saltar de imagen y esta se baya a la izquierda y creando tres variables en donde se realizan operaciones para el control de las imagenes

--- Carrucel_Imagenes/publico/js/main.js
+++ Carrucel_Imagenes/publico/js/main.js
@@ -6,3 +6,11 @@ const sliderSection  = document.querySelectorAll(".slider-section");
 btnLeft.addEventListener("click", e => moveToLeft());
 btnRight.addEventListener("click", e => moveToRight());
 
+setInterval(() => {
+    moveToRight()
+}, 3000);
+
+let operacion = 0;
+let counter = 0;
+let widthImg  = 100 / sliderSection.length;
+
