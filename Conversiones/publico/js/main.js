d470af5533f35c8fa000fd6523780a779378b926
BryanCachimuel <bryanloyo56@gmail.com>
2023-09-14 22:15:17 -0500

    obteniendo los valores dados en las etiquetas o obteniendolos mediante el evento  getElementById

---
+++ Conversiones/publico/js/main.js
@@ -0,0 +1,10 @@
+const entrada = document.getElementById("entrada");
+const boton = document.getElementById("btn");
+const salida = document.getElementById("salida");
+
+let numeroDecimal;
+let divisor;
+let resultadoBinario;
+let cociente;
+let residuo;
+
