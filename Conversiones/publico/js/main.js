8fc4e153bc8be2ac985d25ff1ea462b7579a393d
BryanCachimuel <bryanloyo56@gmail.com>
2023-09-15 19:46:07 -0500

    creando la función convertirDecimalABinario la cual se encarga de realizar los calculos y procesos para hacer la transformación de Decimal a Binario

--- Conversiones/publico/js/main.js
+++ Conversiones/publico/js/main.js
@@ -18,4 +18,18 @@ const validarEntrada = () => {
     (entrada.value === "") ? alert('Ingrese Datos') : console.log('Proceso realizado con Exito');
 }
 
+const convertirDecimalABinario = () => {
+    numeroDecimal = entrada.value;
+    divisor = 2;
+    let numeroSalida = "";
+    while(numeroDecimal >= 2){
+        cociente = Math.trunc(numeroDecimal/divisor);
+        residuo = numeroDecimal % divisor;
+        numeroSalida += residuo.toString();
+        numeroDecimal = cociente;
+    }
+    let auxiliar = numeroDecimal.toString();
+    numeroSalida += auxiliar;
+    return numeroSalida;
+}
 
