b3a2b5891013a60adf95de4aa42a532176dbff77
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-10-17 21:13:57 -0500

    agregando la funcionalidad a la constante closeModal mediante el evento addEventListener

--- Ventana_Modal/publico/js/main.js
+++ Ventana_Modal/publico/js/main.js
@@ -6,3 +6,8 @@ openModal.addEventListener('click', (e)=>{
     e.preventDefault();
     modal.classList.add('modal--show');
 });
+
+closeModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.remove('modal--show');
+});
