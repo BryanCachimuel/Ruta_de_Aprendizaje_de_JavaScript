8a6ba3fc01ea8f18e9f2c987fd587c7b47561e74
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-10-21 19:48:28 -0500

    creando la función rain en donde se hacen los calculos para hacer la caida de textos desde la nube

---
+++ lluvia_de_datos/publico/js/main.js
@@ -0,0 +1,19 @@
+function rain(){
+    let cloud = document.querySelector('.cloud');
+    let e = document.createElement('div');
+    e.classList.add('drop');
+    cloud.appendChild(e);
+
+    let left = Math.floor(Math.random() * 290);
+    let size = Math.random() * 1.5;
+    let duration = Math.random() * 1;
+
+    e.innerText = randomText();
+    e.style.left = left + 'px';
+    e.style.fontSize = 0.5+size+'em';
+    e.style.animationDuration = 1+duration+'s';
+
+    setTimeout(function() {
+        cloud.removeChild('drop');
+    }, 2000);
+}
