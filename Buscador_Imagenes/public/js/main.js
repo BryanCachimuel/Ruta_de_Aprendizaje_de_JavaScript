97beb579ff03d741212584ebd4f8c3e0e25a8f83
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-01-04 21:52:15 -0500

    adecuando el boton para mostrar más imagenes y con ello se despleguen más imagenes relacionadas con la busqueda seleccionada

--- Buscador_Imagenes/public/js/main.js
+++ Buscador_Imagenes/public/js/main.js
@@ -1,77 +1,65 @@
 /* tomo todos los elementos del html */
+//Tomamos todos los elementos del html
 const formBusqueda = document.querySelector("#form-busqueda");
 const cajaBusqueda = document.querySelector("#caja-busqueda");
 const resultadoBusqueda = document.querySelector("#resultado-busqueda");
 const mostrarMas = document.querySelector("#mostrar-mas");
 
-/* variable que guarda la palabra a buscar */
-let keyword = "";
-
-/* número de página de busqueda */
-let page = 1;
+let keyword = "";//variable que guarda la palabra a buscar
+let page = 1; //número de pàgina de busqueda
 
 const accessKey = "4RPJjJ4cvxukR5cs35bG1-p7NxJRYbUzDH75X2gzOg0";
 
-/* función que trae los resultados de las imagenes */
+//Función que trae los resultados
 async function buscarImagenes(){
-
-    /* tomo el valor que es ingresado por el usuario */
+    //tomo el valor que ingresó el usuario
     keyword = cajaBusqueda.value;
+    //armo la url
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
 
-    /* se arma la url */
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
-
-    /* realizo la busqueda */
+    //realizo la busqueda
     const response = await fetch(url);
     const data = await response.json();
 
-    console.log(data);
+    //console.log(data);
 
-    /* 
-        se controla si es la primera vez que se busco. Si es así se 
-        limpia el contenedor
-     */
-        if(page === 1){
-            resultadoBusqueda.innerHTML = "";
-        }
-    
-        /*  coloco los resultados de la busqueda en el contenedor */
-        const resultados = data.results;
-        
-        /* por cada resultado armo un enlace a y adentro le agrego la imagen */
-        resultados.map((result) => {
-            const imagen = document.createElement("img");
-            imagen.src = result.urls.small;
-            const imagenLink = document.createElement("a");
-            imagenLink.href = result.links.html;
-            imagenLink.target = "_blank";
-    
-            /* agrego el elemento */
-            imagenLink.appendChild(imagen);
-            resultadoBusqueda.appendChild(imagenLink);
-    
-            mostrarMas.style.display = "block";
-        })
+    //controlo, si es la primera vez que busco limpio el contendor
+    //donde se muestran los resultados
+    if(page===1){
+        resultadoBusqueda.innerHTML = "";
+    }
+
+    const resultados = data.results;
+    //Por cada resultado armo un enlace a, con la imagen dentro
+    resultados.map((result) => {
+        const imagen = document.createElement("img");
+        imagen.src = result.urls.small;
+        const imagenLink = document.createElement("a");
+        imagenLink.href = result.links.html;
+        imagenLink.target = "_blank";
+
+        imagenLink.appendChild(imagen);
+
+        //agrego el elemento al contendor
+        resultadoBusqueda.appendChild(imagenLink);
+    });
+
+    //muestro el botón mostrar mas
+    mostrarMas.style.display = "block";
+
+
 }
 
-/*
-    agrego funcionalidad para cuando se presione enter 
-    o se haga click en el boton buscar
-*/
-formBusqueda.addEventListener("submit", (e) => {
-
-    /* evito que se recargue la página */
+//Agrego funcionalidad para cuando
+formBusqueda.addEventListener("submit", (e)=>{
+    //Evito que se regarge la pàgina
     e.preventDefault();
     page = 1;
-
-    /* llamo a la función */
     buscarImagenes();
 })
 
-
-mostrarMas.addEventListener("clcik", () => {
+//Funcionalidad al boton Mostrar mas.
+mostrarMas.addEventListener("click", () =>{
     page++;
     buscarImagenes();
 })
-
-
