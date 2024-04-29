de16208cc9387831ebd399be639e10defb8d91c5
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-04-28 22:12:15 -0500

    creando la función populateUI para obtener los datos del localstorage y ponerlos en la interfaz de la sala de cine

--- reserva_cine/public/app.js
+++ reserva_cine/public/app.js
@@ -4,8 +4,15 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI();
 /* anteponiendo el signo + en movieSelect combierte el tipo de dato de string a number */
 let ticketPrice = +movieSelect.value;
+
+/* guardar índice y precio de la película seleccionada */
+function setMovieData(movieIndex, moviePrice){
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
 
 /* función para contar el total */
 function updateSelectedCount(){
@@ -23,6 +30,23 @@ function updateSelectedCount(){
     /* capturando el número de asientos seleccionados y mediante ello se calcula el precio de las entradas y su total respectivo */
     count.innerHTML = selectedSeatsCount;
     total.innerHTML = selectedSeatsCount * ticketPrice;
+}
+
+/* obtener los datos del localstorage y ponerla en la interfaz de la sala de cine */
+function populateUI(){
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    if(selectedSeats !== null && selectedSeats.length > 0){
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1){
+                seat.classList.add('selected');
+            }
+        });
+    } 
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    
+    if(selectedMovieIndex != null){
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
 }
 
 /* evento para seleccionar una de las peliculas del select */
@@ -43,4 +67,6 @@ container.addEventListener('click', (e) => {
     }
 
     updateSelectedCount();
-})
+});
+
+updateSelectedCount();
