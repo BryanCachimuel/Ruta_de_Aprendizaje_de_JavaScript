d05a40c3c8be045cd987f599033b12f85dd4aa96
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-04-28 21:42:06 -0500

    creando un evento para poder seleccionar cada asiento de la sala de cine y creando la función para poder obtener la cantidad de número de asientos y su respectivo total a cancelar por asientos seleccionados

--- reserva_cine/public/app.js
+++ reserva_cine/public/app.js
@@ -4,4 +4,29 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+/* anteponiendo el signo + en movieSelect combierte el tipo de dato de string a number */
+let ticketPrice = +movieSelect.value;
 
+/* función para contar el total */
+function updateSelectedCount(){
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const selectedSeatsCount = selectedSeats.length;
+    //console.log(selectedSeatsCount);
+
+    /* capturando el número de asientos seleccionados y mediante ello se calcula el precio de las entradas y su total respectivo */
+    count.innerHTML = selectedSeatsCount;
+    total.innerHTML = selectedSeatsCount * ticketPrice;
+}
+
+/* captuta el asiento seleccionado */
+container.addEventListener('click', (e) => {
+
+    /* valida que solo se seleccionen los asientos sin ocupar */
+    if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
+       
+        /* pinta de color cian el asiento al seleccionado y dado click */
+        e.target.classList.toggle('selected');
+    }
+
+    updateSelectedCount();
+})
