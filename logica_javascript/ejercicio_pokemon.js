9b68f75d184f18e06f8873ed6f267a6e250f438c
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-07-22 20:15:40 -0500

    Creando la función calcularPromedioHabilidades en donde se hace el script para calcular los valores de las habilidades de cada pokemon

--- logica_javascript/ejercicio_pokemon.js
+++ logica_javascript/ejercicio_pokemon.js
@@ -8,3 +8,15 @@ let habilidades = [
     [80,70,64], // Bulbasur
     [85,90,70] // Squirtle
 ];
+
+function calcularPromedioHabilidades(habilidades){
+    let promedios = [];
+
+    for(let i = 0; i < habilidades.length; i++){
+        let fila = habilidades[i];
+        let suma = fila.reduce((total, habilidad)=>total+habilidad,0);
+        promedios[i] = suma/fila.length;
+    }
+
+    return promedios;
+}
