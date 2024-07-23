19be908adc566595edda47ae178d7e0b904028e8
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-07-22 21:04:30 -0500

    Ejercicio de listado de nombres en lo cual un usuario debe ingresar un nombre a buscar

---
+++ logica_javascript/ejercicios/listado_nombres.js
@@ -0,0 +1,9 @@
+// Lista de nombres fija
+let listadoNombres = ["Andrea","Gabriel","Adriana","Cristofer","Edgar"];
+
+// Usuario puede ingresar un nombre por teclado.
+let nombreBuscar = prompt("Ingresé eñ nombre a buscar: ");
+
+// Busqueda si está o no el nombre en el listado
+
+// Informar por pantalla
