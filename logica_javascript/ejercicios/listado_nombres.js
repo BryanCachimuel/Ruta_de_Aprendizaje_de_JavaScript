3d46cb3b60d9758b5ae4f4eca2a67f15e3828296
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-07-22 21:11:57 -0500

    Creando la función buscarNombre y determinando una variable que suse la función descrita anteriormente

--- logica_javascript/ejercicios/listado_nombres.js
+++ logica_javascript/ejercicios/listado_nombres.js
@@ -4,6 +4,12 @@ let listadoNombres = ["Andrea","Gabriel","Adriana","Cristofer","Edgar"];
 // Usuario puede ingresar un nombre por teclado.
 let nombreBuscar = prompt("Ingresé eñ nombre a buscar: ");
 
+/* Función buscarNombre */
+function buscarNombre(listaNombre, nombreBuscar){
+    return listaNombre.includes(nombreBuscar);
+}
+
 // Busqueda si está o no el nombre en el listado
+let estaEnListado = buscarNombre(listaNombre, nombreBuscar);
 
 // Informar por pantalla
