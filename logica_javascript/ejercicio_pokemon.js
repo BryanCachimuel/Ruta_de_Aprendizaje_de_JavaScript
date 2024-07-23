b294e9d4d6f80303e068009db057edf23868a72b
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-07-22 19:55:18 -0500

    Creando el directorio para repasar lógica de programación con javascript, para este caso se creo un ejercicio sobre pokemon

---
+++ logica_javascript/ejercicio_pokemon.js
@@ -0,0 +1,10 @@
+// Areglo para los nombres
+let nombresPokemon = ['Charmander','Pikachu','Bulbasur','Squirtle'];
+
+// Matriz para sus habilidades
+let habilidades = [
+    [80,75,90], // Charmander
+    [64,55,94], // Pikachu
+    [80,70,64], // Bulbasur
+    [85,90,70] // Squirtle
+];
