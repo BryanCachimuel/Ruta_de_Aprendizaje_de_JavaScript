8616ff591977a09ffa9402c9358a87b01b69e7bd
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-07-28 21:49:32 -0500

    Creando la función generarEnteroAleatorio para poder seleccionar un entero en un intervalo que se establesca por el usuario para poder realizar lo que es la vista de las citas de forma aleatoria

--- Citas_Aleatorias/publico/app.js
+++ Citas_Aleatorias/publico/app.js
@@ -1,3 +1,7 @@
 let botonElem = document.getElementById('boton-cambiar-cita');
 let citaElem = document.getElementById('cita');
 let autorElem = document.getElementById('autor');
+
+function generarEnteroAleatorio(min, max){
+    return Math.floor(Math.random() * (max - min) + min);
+}
