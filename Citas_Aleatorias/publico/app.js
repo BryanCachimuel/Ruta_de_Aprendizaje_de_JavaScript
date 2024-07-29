0dfc1417e461fdea36f140aef4d89d56f402288b
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-07-28 22:01:44 -0500

    Creando la función cambiarCita en donde se elige aletatoriamente la cita y el nombre del autor respectivamente

--- Citas_Aleatorias/publico/app.js
+++ Citas_Aleatorias/publico/app.js
@@ -3,5 +3,15 @@ let citaElem = document.getElementById('cita');
 let autorElem = document.getElementById('autor');
 
 function generarEnteroAleatorio(min, max){
+    // Sin incluir max en los valores posibles
     return Math.floor(Math.random() * (max - min) + min);
 }
+
+function cambiarCita(){
+    let indiceAleatorio = generarEnteroAleatorio(0, citas.length);
+    citaElem.innerHTML = `"${citas[indiceAleatorio].texto}"`;
+    autorElem.innerHTML = citas[indiceAleatorio].autor;
+}
+
+
+cambiarCita();
