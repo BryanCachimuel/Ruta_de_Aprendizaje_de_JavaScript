c59cb570a7572cde3a3ca4b2f886530aaaf67e7b
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-08-19 21:28:01 -0500

    Extrayendo los nombres de los identificadores y clases del reproducto

---
+++ Reproductor_musica/public/app.js
@@ -0,0 +1,11 @@
+const tituloCancion = document.querySelector('.titulo');
+const nombreArtista = document.querySelector('.nombre');
+
+const progreso = document.getElementById('progreso');
+const cancion = document.getElementById('cancion');
+
+const iconoControl = document.getElementById('iconoControl');
+const botonReproducirPausar = document.querySelector('.controles buton-reproducir-pausar');
+
+const botonAtras = document.querySelector('.controles button.atras');
+const botonAdelante = document.querySelector('.controles button.adelante');
