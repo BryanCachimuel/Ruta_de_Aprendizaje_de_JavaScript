56599ff35ca6468155ccb12d1ccef1359b9e5680
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-08-19 21:54:05 -0500

    Agregando un objeto canciones y una función para actualizaInformación

--- Reproductor_musica/public/app.js
+++ Reproductor_musica/public/app.js
@@ -9,3 +9,27 @@ const botonReproducirPausar = document.querySelector('.controles buton-reproduci
 
 const botonAtras = document.querySelector('.controles button.atras');
 const botonAdelante = document.querySelector('.controles button.adelante');
+
+const canciones = [
+   {
+    titulo:'A Year Ago',
+    nombre:'NETFEX', 
+    fuente:'music/A year Ago - NEFFEX.MPS'
+   },
+   {
+    titulo:'A Year Ago',
+    nombre:'NETFEX', 
+    fuente:'music/As You Fade Away NETFFE.MP3'
+   }
+];
+
+let indiceCancionActual = 1;
+
+function actualizarInformacion(){
+    tituloCancion.textContent = canciones[indiceCancionActual].titulo;
+    nombreArtista.textContent = canciones[indiceCancionActual].nombre;
+    cancion.src = canciones[indiceCancionActual].fuente;
+    cancion = addEventListener('loadeddata', function(){});
+}
+
+actualizarInfoCancion();
