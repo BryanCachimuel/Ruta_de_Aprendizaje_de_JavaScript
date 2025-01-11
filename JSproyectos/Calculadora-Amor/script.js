d8a2185efc8019e133e529daae409c9865da3255
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-01-10 22:35:59 -0500

    Realizando una función para que me calcule el porcentaje de compatibilidad entre dos personas

---
+++ JSproyectos/Calculadora-Amor/script.js
@@ -0,0 +1,15 @@
+function calcularAmor(){
+    const nombre1 = document.getElementById("nombre1").value
+    const nombre2 = document.getElementById("nombre2").value
+    
+    if(nombre1 && nombre2){
+        const hash = (nombre1 + nombre2).split('').reduce((acc, char) => acc + char.charCodeAt(0), 0)
+        const amorPorcentaje = hash % 101
+        document.getElementById("resultados").style.color = 'green'
+        document.getElementById("resultados").textContent = `${nombre1} tienes un ${amorPorcentaje}% de compatibilidad con ${nombre2}`
+    }
+    else{
+        document.getElementById("resultados").style.color = 'red'
+        document.getElementById("resultados").textContent = 'Ingrese ambos nombres por favor'
+    }
+}
