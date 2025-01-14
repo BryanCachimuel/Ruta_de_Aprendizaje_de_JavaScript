6de924d91895c28810b67affe5b34925b59f41d8
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-01-13 22:41:12 -0500

    Realizando el procedimiento para listar los contactos de la agenda como también la creación de un boton de eliminación

--- JSproyectos/Agenda-Contactos/app.js
+++ JSproyectos/Agenda-Contactos/app.js
@@ -1,9 +1,17 @@
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-  
+document.getElementById("contactForm").addEventListener("submit", function (e) {
   e.preventDefault();
 
   const name = document.getElementById("contactName").value;
   const tel = document.getElementById("contactTel").value;
 
+  const listItem = document.createElement("li");
+  listItem.textContent = `${name} - ${tel} `;
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Eliminar";
+  deleteBtn.addEventListener("click", function () {
+    listItem.remove();
+  });
+  listItem.appendChild(deleteBtn);
 
-})
+  document.getElementById("contactList").appendChild(listItem);
+});
