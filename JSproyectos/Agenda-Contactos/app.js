59e525e4f78fa44108d9b496a2e61a533d1c496d
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-01-13 22:38:47 -0500

    Obteniendo el formulario del maquetado al cual se le aplicará el método addEvenListener para realizar el proceso de agendamiento

---
+++ JSproyectos/Agenda-Contactos/app.js
@@ -0,0 +1,9 @@
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  
+  e.preventDefault();
+
+  const name = document.getElementById("contactName").value;
+  const tel = document.getElementById("contactTel").value;
+
+
+})
