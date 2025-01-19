0908ce09bb1fecece424b0df7a939eb3e5e0cf26
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-01-18 22:57:16 -0500

    Realizando un arreglo de objetos el cual cuenta con las adivinanzas para el juego

---
+++ JSproyectos/Adivinanzas/script.js
@@ -0,0 +1,16 @@
+const riddles = [
+  {
+    question: "Se mira pero no se toca. ¿Qué es?",
+    answer: "espejo",
+  },
+
+  {
+    question: "Blanco por dentro, verde por fuera. Si quieres que te lo diga, espera. ¿Qué es?",
+    answer: "pera",
+  },
+
+  {
+    question: "¿Qué tiene llaves pero no abre puertas?",
+    answer: "piano",
+  },
+];
