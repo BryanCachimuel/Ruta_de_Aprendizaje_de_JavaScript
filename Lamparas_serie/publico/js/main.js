b4d4028d7fa16ae0b066e3c9eeab7cb1c131295f
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-15 22:18:57 -0500

    Eliminando archivos de funcionamiento con javascript que no deben ser utilizados en el proyecto

--- Lamparas_serie/publico/js/main.js
+++
@@ -1,8 +0,0 @@
-let btn = document.querySelector('.btn');
-let body = document.querySelector('body');
-let sound = document.getElementById('audio');
-
-btn.onclick = function(){
-    body.classList.toggle('on');
-    sound.play();
-}
