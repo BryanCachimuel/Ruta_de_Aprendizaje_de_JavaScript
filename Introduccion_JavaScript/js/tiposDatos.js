71a8b3444efbee19262a04f6021baf286706c48c
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-22 22:21:32 -0500

    Estudiando los tipos de datos en JavaScript

---
+++ Introduccion_JavaScript/js/tiposDatos.js
@@ -0,0 +1,23 @@
+/*
+    Tipos de Datos 
+*/
+
+// Tipo de dato indefinido -> al no asiganr un valor esta variable tine un valor undefined
+let indefinido;
+console.log(indefinido);
+
+// Tipo cadena de caracter
+let texto = 'Cadena de Texto'
+console.log(texto + ":  Tipo de Dato-> " + typeof(texto));
+
+// Tipo entero
+let entero = 30;
+console.log(entero +":  Tipo de Dato-> "+ typeof(entero));
+
+// Tipo Decimal
+let decimal = 19.75
+console.log(decimal + ": Tipo de Dato->" + typeof(decimal));
+
+// Tipos de Datos Booleanos
+console.log(typeof(true));
+console.log(typeof(false));
