614bc744b83d6e82f273e95d2c1fac759ad2fe58
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-22 22:09:36 -0500

    Aprendiendo los tipos de variables y constantes que existen en JavaScript

---
+++ Introduccion_JavaScript/js/variables.js
@@ -0,0 +1,15 @@
+/*
+    Definición de variables
+*/
+let nombre = 'BLCL';
+
+console.log(nombre);
+
+// buenas practicas al dar un nombre a una variable
+let nombreCompleto = 'Nelson Cacoango';
+console.log(nombreCompleto);
+
+// constantes -> no pueden cambiar su valor y se definen con letras mayusculas por convención y para distinguirse de las variables
+const PAIS = 'Ecuador'
+console.log(PAIS);
+
