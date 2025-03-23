449cc9b9a9b7ef139ca560f4e639440c68bd4f1f
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-22 22:39:13 -0500

    Realizando el ejercicio de la suma de dos números

---
+++ Introduccion_JavaScript/js/ejercicioDatos.js
@@ -0,0 +1,9 @@
+/*
+    Suma de dos números
+*/
+
+let valorUno = 14;
+let valorDos = 20;
+
+let suma = Number(valorUno) + Number(valorDos);
+console.log(`La suma del valorUno: ${valorUno} + valorDos: ${valorDos} es: ${suma}`)
