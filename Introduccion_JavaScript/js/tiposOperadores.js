eccc493b35cd0e3ee0e83d7a68220af42560c3cb
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-23 18:57:38 -0500

    Apriendo la forma de usar los operadores aritméticos

---
+++ Introduccion_JavaScript/js/tiposOperadores.js
@@ -0,0 +1,21 @@
+/*
+    Tipos de Operadores
+*/
+
+// Operadores Aritméticos
+let valor1 = 21; 
+let valor2 = 12;
+
+let suma = valor1 + valor2;
+let resta = valor1 - valor2;
+let multiplicacion = valor1 * valor2;
+let division = valor1 / valor2;
+let modulo = valor1 % valor2;
+let potenciacion = valor1 ** valor2;
+
+console.log(suma)
+console.log(resta)
+console.log(multiplicacion)
+console.log(division)
+console.log(modulo)
+console.log(potenciacion)
