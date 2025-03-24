a88ec428a0653c8ae482c8038a8b051e7a99ce07
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-23 22:32:01 -0500

    Aplicando los operadores aritméticos se realizá el ejercicio de obtener el cociente y residuo de una operación entre dos números

---
+++ Introduccion_JavaScript/js/ejercicioCociente.js
@@ -0,0 +1,10 @@
+let valor1 = prompt('Ingrese el primer número: ')
+let valor2 = prompt('Ingrese el segundo número: ')
+
+let cociente = parseInt(valor1) / parseInt(valor2);
+
+let residuo = parseInt(valor1) % parseInt(valor2)
+
+document.writeln(cociente)
+document.writeln("<br>")
+document.writeln(residuo)
