0d631609e08ee35308d00a766b34b8dab8bc491a
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-23 19:51:13 -0500

    Apriendo la forma de usar los operadores de comparación

--- Introduccion_JavaScript/js/tiposOperadores.js
+++ Introduccion_JavaScript/js/tiposOperadores.js
@@ -28,34 +28,63 @@ console.log(potenciacion)*/
 let valor3 = 15;
 valor3 += 12;
 
-console.log(valor3)
+/*console.log(valor3)*/
 
 // resta en asignación -=
 let valor4 = 42;
 valor4 -= 8;
 
-console.log(valor4)
+/*console.log(valor4)*/
 
 // multiplicación en asignación *=
 let valor5 = 55;
 valor5 *= 3;
 
-console.log(valor5)
+/*console.log(valor5)*/
 
 // división en asignación /=
 let valor6 = 80;
 valor6 /= 8;
 
-console.log(valor6)
+/*console.log(valor6)*/
 
 // potenciación en asignación **=
 let valor7 = 6;
 valor7 **= 4;
 
-console.log(valor7)
+/*console.log(valor7)*/
 
 // modulo en asignación %=
 let valor8 = 25;
 valor8 %= 3;
 
-console.log(valor8)
+/*console.log(valor8)*/
+
+/*
+    Operadores de Comparación
+*/
+
+// Comparación de tipos de datos
+let numero1 = 5
+let numero2 = '5'
+let numero3 = 6
+let letras = "a"
+let letras2 = "A"
+
+let resultado = numero1 == numero2
+let resultado2 = numero1 === numero2
+let resultado3 = numero1 != numero3
+let resultado4 = numero1 !== numero2
+let resultado5 = numero1 > numero3
+let resultado6 = numero1 < numero3
+let resultado7 = numero1 <= numero3
+let resultado8 = numero1 >= numero3
+let resultado9 = letras == letras2
+
+console.log(resultado)
+console.log(resultado2)
+console.log(resultado3)
+console.log(resultado4)
+console.log(resultado5)
+console.log(resultado6)
+console.log(resultado9)
