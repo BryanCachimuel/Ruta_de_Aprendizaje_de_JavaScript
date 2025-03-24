411bd0f674bbbe40c2e4019e90d3e25ec0478d63
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-23 19:06:51 -0500

    Apriendo la forma de usar los operadores en asignación

--- Introduccion_JavaScript/js/tiposOperadores.js
+++ Introduccion_JavaScript/js/tiposOperadores.js
@@ -13,9 +13,49 @@ let division = valor1 / valor2;
 let modulo = valor1 % valor2;
 let potenciacion = valor1 ** valor2;
 
-console.log(suma)
+/*console.log(suma)
 console.log(resta)
 console.log(multiplicacion)
 console.log(division)
 console.log(modulo)
-console.log(potenciacion)
+console.log(potenciacion)*/
+
+/*
+    Operadores de Asiganción
+*/
+
+// suma en asignación +=
+let valor3 = 15;
+valor3 += 12;
+
+console.log(valor3)
+
+// resta en asignación -=
+let valor4 = 42;
+valor4 -= 8;
+
+console.log(valor4)
+
+// multiplicación en asignación *=
+let valor5 = 55;
+valor5 *= 3;
+
+console.log(valor5)
+
+// división en asignación /=
+let valor6 = 80;
+valor6 /= 8;
+
+console.log(valor6)
+
+// potenciación en asignación **=
+let valor7 = 6;
+valor7 **= 4;
+
+console.log(valor7)
+
+// modulo en asignación %=
+let valor8 = 25;
+valor8 %= 3;
+
+console.log(valor8)
