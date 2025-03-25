a16517c661abaff87c6866f90a945aa9f7d525ad
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-24 22:45:46 -0500

    Aprendiendo a usar la estructura de control switch

---
+++ Introduccion_JavaScript/js/estructurasControl/switch.js
@@ -0,0 +1,35 @@
+let dia = 'Martes';
+
+switch(dia){
+    case 'Lunes':
+        console.log(`El día: ${dia}, si es un día de la semana`)
+        break;
+    
+    case 'Martes':
+        console.log(`El día: ${dia}, si es un día de la semana`)
+        break;
+
+    case 'Miércoles':
+        console.log(`El día: ${dia}, si es un día de la semana`)
+        break;
+    
+    case 'Jueves':
+        console.log(`El día: ${dia}, si es un día de la semana`)
+        break;
+
+    case 'Viernes':
+        console.log(`El día: ${dia}, si es un día de la semana`)
+        break;
+
+    case 'Sábado':
+        console.log(`El día: ${dia}, si es un día de la semana`)
+        break;
+    
+    case 'Domingo':
+         console.log(`El día: ${dia}, si es un día de la semana`)
+         break;
+
+    default:
+        console.log(`${dia}: No es un día de la semana`)
+        break;
+}
