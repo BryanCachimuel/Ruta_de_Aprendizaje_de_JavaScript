6075000e79de50fd4a8bfecffda9bbc550c942a3
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-24 22:11:06 -0500

    Aprendiendo a usar las estructuras de control if y else

---
+++ Introduccion_JavaScript/js/estructurasControl.js
@@ -0,0 +1,11 @@
+/*
+    Estructuras de Control if-else
+*/
+
+let valor = 23;
+
+if(valor % 2 === 0){
+    console.log(`El valor ${valor} es par`)
+} else {
+    console.log(`El valor ${valor} es impar`)
+}
