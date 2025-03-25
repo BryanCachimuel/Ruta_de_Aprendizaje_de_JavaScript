ad1b092f8d5c25b30ed2ac73adf5a1b0d976655c
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-24 22:18:20 -0500

    Aprendiendo a usar las estructuras de control if y else de forma anidada

--- Introduccion_JavaScript/js/estructurasControl.js
+++ Introduccion_JavaScript/js/estructurasControl.js
@@ -9,3 +9,24 @@ if(valor % 2 === 0){
 } else {
     console.log(`El valor ${valor} es impar`)
 }
+
+// Condiciones anidadas
+let valor2 = 28;
+   
+if(valor2 !== 0){
+    if(valor2 > 0){
+        if(valor2 % 2 === 0){
+            console.log(`El valor ${valor2} es par positivo`)
+        } else {
+            console.log(`El valor ${valor2} es impar positivo`)
+        }
+    }else{
+        if(valor2 % 2 === 0){
+            console.log(`El valor ${valor2} es par negativo`)
+        } else {
+            console.log(`El valor ${valor2} es impar negativo`)
+        }
+    }
+}else{
+    console.log(`El valor ${valor2} es neutro`)
+}
