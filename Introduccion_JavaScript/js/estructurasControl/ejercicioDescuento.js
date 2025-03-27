40497aafe04a6fefc8b5a351d98ffd4abbde2714
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-26 22:37:01 -0500

    Realizando la práctica 3 donde se debia hacer la multiplicación de los elementos de un arreglo por números aleatorios del 1 a 100 y luego cada valor almacenarlo en dos arrays para clasificarlos en pares e impares

--- Introduccion_JavaScript/js/estructurasControl/ejercicioDescuento.js
+++ Introduccion_JavaScript/js/estructurasControl/ejercicioDescuento.js
@@ -1,5 +1,5 @@
 /*
-    Practica 01: Descuentos de un restaurante
+    Practica 1: Descuentos de un restaurante
 
     Enunciado: un restaurante ofrece un descuento del 10% para consumo de hasta $100.00 y un descuento del 20 % para consumos mayores, 
     para ambos casos se aplica un impuesto del 19%. Determinar el monto del descuento, el impuesto y el valor total a pagar.
@@ -67,5 +67,43 @@ else{
     ValorConsumo: ${valorConsumo}
     Impuesto: ${impuesto}
     Valor Total: ${valorTotal}`);
+}
+
+/*
+    Practica 2: Guardar resultados de pares e impares
+
+    Crea 3 arrays y un array que tendrá números de 1 a 9. El primera array se llamará pares y el segundo impar, 
+    ambos estarán vacíos. Después multiplica cada uno de los números de array con números por un número aleatorio entre 1 y 100, 
+    si el resultado es par guarda ese número en el arrya de pares y si es impar en el array  de impares. Muestra por consola:
+     -la multiplicación que se produce junto con su resultado con el formato 2 x 3 = 6 y el array de pares e impares 
+*/
+
+let numeros = [1,2,3,4,5,6,7,8,9];
+let numeroAleatorios = 1;
+let valoresMultiplicacion = 0;
+let pares = []
+let impares = [];
+
+
+for(let i = 0; i < numeros.length; i++){
+    numeroAleatorios = Math.floor(Math.random() * 101);
+    valoresMultiplicacion = numeros[i] * numeroAleatorios;
+   console.log(`${numeros[i]} x ${numeroAleatorios} = ${numeros[i] * numeroAleatorios}`)
+    if(valoresMultiplicacion%2 === 0){
+        pares.push(valoresMultiplicacion);        
+    }else{
+        impares.push(valoresMultiplicacion);   
+    }
+}
+
+console.log('\n')
+for(let valoresPares of pares){
+    console.log(valoresPares)
+}
+
+console.log('\n')
+
+for(let valoresImpares of impares){
+    console.log(valoresImpares)
 }
 
