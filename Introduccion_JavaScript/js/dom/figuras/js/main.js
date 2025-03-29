9e2f8cc7b983b2b495c3d35f1c4a3a498165fdf4
BryanCachimuel <bryanloyo56@gmail.com>
2025-03-28 23:14:41 -0500

    Creando una función para realizar el procedimiento de impresión de resultados y con ello poder realizar el llamado de la función desde el boton calcular calcular area

--- Introduccion_JavaScript/js/dom/figuras/js/main.js
+++ Introduccion_JavaScript/js/dom/figuras/js/main.js
@@ -1,3 +1,5 @@
+
+let valorResultado = document.getElementById('result')
 /* calcular el perimetro */
 const calcularPerimetro = (lado) => lado * 4;
 
@@ -10,6 +12,13 @@ function calcularPerimetroCuadrado(){
     let valor = Number(inputSide.value);
 
     const resultado = `El perímetro del cuadrado es: ${calcularPerimetro(valor)} cm`
+    valorResultado.innerHTML = resultado
+}
 
-    document.getElementById('result').innerHTML = resultado
+function calcularAreaCuadrado(){
+    let inputSide = document.querySelector('#inputSide')
+    let valor = Number(inputSide.value);
+
+    const resultado = `El área del cuadrado es: ${calcularArea(valor)} cm^2`
+    valorResultado.innerHTML = resultado 
 }
