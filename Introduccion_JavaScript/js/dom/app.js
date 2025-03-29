370f3beff585473d11c43411d802edb0e84d610a
BryanCachimuel <bryanloyo56@gmail.com>
2025-03-28 22:31:47 -0500

    Aprendindo a manipular etiquetas mediante el uso de variables

--- Introduccion_JavaScript/js/dom/app.js
+++ Introduccion_JavaScript/js/dom/app.js
@@ -5,3 +5,22 @@ document.querySelector('title').innerText = 'DOM'
 
 // Ingresando en el dom dentro del body un mensaje
 document.querySelector('body').innerHTML = '<h1>Saludos desde JavaScript</h1>'
+
+// Obtener una clase definida 
+document.querySelectorAll('.caja')
+document.getElementsByClassName('caja')
+
+// Obtener por id definida en una etiqueta
+document.querySelector('#caja1')
+document.getElementById('caja1')
+
+// Manipular un elemento mediante el uso de variables
+let caja1 = document.querySelector('#caja1')
+
+// crear un elemento dentro de documento
+let btn = document.createElement('button')
+caja1.append(btn)
+btn.innerText = 'Boton'
+
+// agregar una clase al boton creado
+btn.classList.add('btn')
