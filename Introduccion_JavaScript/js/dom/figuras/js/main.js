1ed012a1e0eaeb224c649f8b11e16c1fe22d63e0
BryanCachimuel <bryanloyo56@gmail.com>
2025-03-28 23:06:59 -0500

    Creando una función para realizar el procedimiento de impresión de resultados y con ello poder realizar el llamado de la función desde el boton calcular perímetro

--- Introduccion_JavaScript/js/dom/figuras/js/main.js
+++ Introduccion_JavaScript/js/dom/figuras/js/main.js
@@ -1,8 +1,15 @@
-let inputCuadrado = document.querySelector('#side')
-let valor = inputCuadrado.value;
-
 /* calcular el perimetro */
 const calcularPerimetro = (lado) => lado * 4;
 
 /* calcular el arear */
 const calcularArea =( lado) => lado * lado;
+
+/* para darle funcionalidad al boton perimetro */
+function calcularPerimetroCuadrado(){
+    let inputSide = document.querySelector('#inputSide')
+    let valor = Number(inputSide.value);
+
+    const resultado = `El perímetro del cuadrado es: ${calcularPerimetro(valor)} cm`
+
+    document.getElementById('result').innerHTML = resultado
+}
