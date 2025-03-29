cb745f1dafae48b1a3f35c679d0d7cc6a9e9ed39
BryanCachimuel <bryanloyo56@gmail.com>
2025-03-28 22:53:58 -0500

    Realizando las funciones para calcular el área y el perímetro de un cuadrado

---
+++ Introduccion_JavaScript/js/dom/figuras/js/main.js
@@ -0,0 +1,8 @@
+let inputCuadrado = document.querySelector('#side')
+let valor = inputCuadrado.value;
+
+/* calcular el perimetro */
+const calcularPerimetro = (lado) => lado * 4;
+
+/* calcular el arear */
+const calcularArea =( lado) => lado * lado;
