bdfafc400e69afcb951b8650f0c32b580c7e4bee
BryanCachimuel <bryanloyo56@gmail.com>
2025-03-28 22:08:07 -0500

    Aprendiendo a manipular el DOM

---
+++ Introduccion_JavaScript/js/dom/app.js
@@ -0,0 +1,7 @@
+console.log(document.querySelectorAll('div'))
+
+// cambiando el dato puesto en el título de la pestaña
+document.querySelector('title').innerText = 'DOM'
+
+// Ingresando en el dom dentro del body un mensaje
+document.querySelector('body').innerHTML = '<h1>Saludos desde JavaScript</h1>'
