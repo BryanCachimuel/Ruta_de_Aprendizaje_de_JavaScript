df6aeb3126d53b04ff5841a7f2c9ec54f212cf3f
BryanCachimuel <bryanloyo56@gmail.com>
2025-03-29 20:19:31 -0500

    Calculando el área del triangulo

--- Introduccion_JavaScript/js/dom/figuras/js/main.js
+++ Introduccion_JavaScript/js/dom/figuras/js/main.js
@@ -1,12 +1,13 @@
 
 let valorResultado = document.getElementById('result')
+
 /* calcular el perimetro */
 const calcularPerimetro = (lado) => lado * 4;
 
 /* calcular el arear */
 const calcularArea =( lado) => lado * lado;
 
-/* para darle funcionalidad al boton perimetro */
+/* para darle funcionalidad al boton perimetro del cuadradp*/
 function calcularPerimetroCuadrado(){
     let inputSide = document.querySelector('#inputSide')
     let valor = Number(inputSide.value);
@@ -15,10 +16,32 @@ function calcularPerimetroCuadrado(){
     valorResultado.innerHTML = resultado
 }
 
+/* para darle funcionalidad al boton del área del cuadrado */
 function calcularAreaCuadrado(){
     let inputSide = document.querySelector('#inputSide')
     let valor = Number(inputSide.value);
 
     const resultado = `El área del cuadrado es: ${calcularArea(valor)} cm^2`
     valorResultado.innerHTML = resultado 
+}
+
+const perimetroTrianglo = (lado1, lado2, base) => lado1 + lado2 + base;
+
+const areaTriangulo = (base, altura) => (base * altura)/2;
+
+function calcularPerimetroTriangulo(){
+    let inputSide1 = Number(document.querySelector('#inputSide1').value);
+    let inputSide2 = Number(document.querySelector('#inputSide2').value);
+    let inputSide3 = Number(document.querySelector('#inputSide3').value);
+
+    const resultado = `El perìmetro del triangulo es: ${perimetroTrianglo(inputSide1, inputSide2, inputSide3)} cm`
+    valorResultado.innerHTML = resultado
+}
+
+function calcularAreaTriangulo(){
+    let inputSide4 = Number(document.querySelector('#inputSide4').value);
+    let inputSide5 = Number(document.querySelector('#inputSide5').value);
+
+    const resultado = `El área del triangulo es ${areaTriangulo(inputSide4, inputSide5)} cm^2`
+    valorResultado.innerHTML = resultado
 }
