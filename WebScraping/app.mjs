4fed6860c29419bf4c8a1e2eaea9c66dd64bad26
BryanCachimuel <bryanloyo56@gmail.com>
2025-06-06 22:23:13 -0500

    Realizando una validación para ver si existe tarjetas dentro de la página web y si las hay me permite recorrer todas las tarejas existentes y las imprime por consola

--- WebScraping/app.mjs
+++ WebScraping/app.mjs
@@ -1,7 +1,26 @@
 import { chromium } from "playwright";
 
-const browser = await chromium.launch({headless:true});
+const browser = await chromium.launch({channel:"chrome",headless:true});
 
 const page = await browser.newPage();
 
-await page.goto("https://uskokrum2010.com/public/index");
+await page.goto("https://uskokrum2010.com/public/index", {waitUntil:"domcontentloaded"});
+
+await page.waitForSelector(".videoCard");
+
+const exists = await page.$(".videoCard");
+
+if(!exists) {
+    console.log("No se encontro ningunha tarjeta");
+} else {
+    const cards = await page.$$eval(".videoCard", (results) =>
+        results.map((element) => {
+            return {Datos: "de tarjetas"}
+        })
+    );
+
+    console.log(cards)
+
+    await browser.close();
+}
+
