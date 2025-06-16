44d808f7e2d17e1b99494b0e64aee9aa6f1dae69
BryanCachimuel <bryanloyo56@gmail.com>
2025-06-15 22:35:32 -0500

    Creando la función runCode y mandando a llamar a la misma dentro del evento que ejecuta el boton del proyecto

--- Ejercicios_Manipulacion_DOM/JSproyectos/Editor-Codigo/app.js
+++ Ejercicios_Manipulacion_DOM/JSproyectos/Editor-Codigo/app.js
@@ -1,5 +1,19 @@
 /* Obtener los elementos del DOM */
 
 const codeEditor = document.getElementById("code-editor")
-const outputDiv = document.getElementById("output")
+const outputDiv = document.getElementsByClassName("output")
 const runButton = document.getElementById("run-button")
+
+runButton.addEventListener("click", runCode)
+
+/* Se define la función */
+function runCode(){
+    const code = codeEditor.value;
+
+    try {
+        const resultado = eval(code)
+        outputDiv.textContent = resultado
+    } catch (error) {
+        outputDiv.textContent = `Error: ${error.message}`
+    }
+}
