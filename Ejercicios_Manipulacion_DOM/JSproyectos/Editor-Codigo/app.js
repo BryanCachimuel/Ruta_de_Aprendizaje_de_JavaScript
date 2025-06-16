f014c71ed989aaea34ea894bca0ed2d9c985ccec
BryanCachimuel <bryanloyo56@gmail.com>
2025-06-15 22:25:40 -0500

    Obteniendo los elementos que se va a obtener del DOM

---
+++ Ejercicios_Manipulacion_DOM/JSproyectos/Editor-Codigo/app.js
@@ -0,0 +1,5 @@
+/* Obtener los elementos del DOM */
+
+const codeEditor = document.getElementById("code-editor")
+const outputDiv = document.getElementById("output")
+const runButton = document.getElementById("run-button")
