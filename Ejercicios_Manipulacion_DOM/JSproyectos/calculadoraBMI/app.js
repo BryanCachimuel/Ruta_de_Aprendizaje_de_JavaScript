b9eb9dab740fbe4f85bbbaff919132ceecb2f2d1
BryanCachimuel <bryanloyo56@gmail.com>
2025-06-23 12:14:16 -0500

    Mediante una condición if y else se realiza el calculo para obtener el BMI de un usuario

--- Ejercicios_Manipulacion_DOM/JSproyectos/calculadoraBMI/app.js
+++ Ejercicios_Manipulacion_DOM/JSproyectos/calculadoraBMI/app.js
@@ -1,4 +1,13 @@
 function calculateBMI() {
     const weight = parseFloat(document.getElementById("weight").value)
     const height = parseFloat(document.getElementById("height").value) / 100
+
+    if(weight && height) {
+        const bmi = (weight / (height * height)).toFixed(2)
+        document.getElementById("result").style.color = '#0854c7'
+        document.getElementById("result").textContent = `Tu BMI es ${bmi}`
+    }else {
+        document.getElementById("result").style.color = '#eb0909'
+        document.getElementById("result").textContent = `Introduce Valores Validos`
+    }
 }
