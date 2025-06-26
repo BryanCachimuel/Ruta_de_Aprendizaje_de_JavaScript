2ed65f9628277acb2d2079dbb8b051547bdf4093
BryanCachimuel <bryanloyo56@gmail.com>
2025-06-25 22:58:22 -0500

    Agregando variables que serviran como caracteres disponibles para formar las contraseñas

--- Ejercicios_Manipulacion_DOM/JSproyectos/Generador-Contrasenias/publico/js/main.js
+++ Ejercicios_Manipulacion_DOM/JSproyectos/Generador-Contrasenias/publico/js/main.js
@@ -4,3 +4,12 @@ const btnGenerar = document.querySelector(".btn-generar");
 const btnLimpiar = document.querySelector(".btn-limpiar");
 const contrasenia = document.querySelector(".contraseña");
 const alerta = document.querySelector(".alerta");
+
+// Caracteres disponibles para la contraseña
+MAYUSCULAS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+MINUSCULAS = "abcdefghijklmnopqrstuvwxyz"
+NUMEROS = "0123456789"
+ESPECIALES = "!@#$%^&*()"
+
+const caracteres = MAYUSCULAS + MINUSCULAS + NUMEROS + ESPECIALES
+
