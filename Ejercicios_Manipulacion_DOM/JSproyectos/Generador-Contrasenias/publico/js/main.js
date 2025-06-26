83dbed2a59e984e1e0125aa8102102712af7bff0
BryanCachimuel <bryanloyo56@gmail.com>
2025-06-25 22:54:52 -0500

    Seleccionando los elementos del maquetado mediante document.querySelector

---
+++ Ejercicios_Manipulacion_DOM/JSproyectos/Generador-Contrasenias/publico/js/main.js
@@ -0,0 +1,6 @@
+// selección de elemetos
+const cantidad = document.querySelector(".cantidad");
+const btnGenerar = document.querySelector(".btn-generar");
+const btnLimpiar = document.querySelector(".btn-limpiar");
+const contrasenia = document.querySelector(".contraseña");
+const alerta = document.querySelector(".alerta");
