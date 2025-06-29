e568d7f7461e08c2eae59e73cd3518514e9bbce3
BryanCachimuel <bryanloyo56@gmail.com>
2025-06-28 22:37:40 -0500

    Seleccionado mediante una clase los elementos del maquetado para trabajar en su respectiva funcionalidad

---
+++ Ejercicios_Manipulacion_DOM/JSproyectos/Generador-Codigos-QR/pubico/js/main.js
@@ -0,0 +1,6 @@
+/* Selección de Elementos */
+const textInput = document.querySelector(".text-input");
+const botonGenerador = document.querySelector(".btn-generar");
+const contenedorQR = document.querySelector(".qrcode");
+const botonDescargar = document.querySelector(".btn-descargar");
+const aviso = document.querySelector(".text-aviso");
