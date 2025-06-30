36bb178776dcbcce3fe7b625219f1b6b9fac1f3f
BryanCachimuel <bryanloyo56@gmail.com>
2025-06-30 12:19:46 -0500

    Creando la funcionalidad para el boton botonGenerar

--- Ejercicios_Manipulacion_DOM/JSproyectos/Generador-Codigos-QR/pubico/js/main.js
+++ Ejercicios_Manipulacion_DOM/JSproyectos/Generador-Codigos-QR/pubico/js/main.js
@@ -1,6 +1,20 @@
 /* Selección de Elementos */
 const textInput = document.querySelector(".text-input");
-const botonGenerador = document.querySelector(".btn-generar");
+const botonGenerar = document.querySelector(".btn-generar");
 const contenedorQR = document.querySelector(".qrcode");
 const botonDescargar = document.querySelector(".btn-descargar");
 const aviso = document.querySelector(".text-aviso");
+
+/* Principal */
+botonGenerar.addEventListener("click", e => {
+    e.preventDefault();
+    const texto = textInput.value;
+    
+    if(!texto) {
+        mostrarAviso("No has ingresado ningun texto.");
+    } else {
+        generarCodigoQr(texto);
+        botonDescargar.style.display = "block";
+    }
+
+})
