dbd5e6d088f3ea556723a11b9f5a8e42e9eca448
BryanCachimuel <bryanloyo56@gmail.com>
2025-07-03 21:01:51 -0500

    Agregando las variables globales e insertando una columna para los botones de editar, eliminar aceptar y cancelar

---
+++ Ejercicios_Manipulacion_DOM/JSproyectos/CRUD-Pokemon/publico/js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/* Variobles globales */
+let params = null;
+let colsEdit = null;
+
+const colEditHtml = `
+    <td>
+        <div class="btn-group">
+            <button id="bEdit" class="btn btn-edit" onClick="rowEdit(this);"><i class="fas fa-edit"></i></button>
+            <button id="bElim" class="btn btn-delete" onClick="rowElim(this);"><i class="fas fa-trash"></i></button>
+            <button id="bAcep" class="btn btn-accept" onClick="rowAcept(this);"><i class="fas fa-check"></i></button>
+            <button id="bCanc" class="btn btn-cancel" onClick="rowCancel(this);"><i class="fas fa-times"></i></button>
+        </div>
+    </td>
+`;
