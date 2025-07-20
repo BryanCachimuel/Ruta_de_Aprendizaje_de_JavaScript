1f453d2e80dc51072360f1891dee7a50561dea49
BryanCachimuel <bryanloyo56@gmail.com>
2025-07-19 22:27:40 -0500

    Creando materiales y un material transparente para caras adicionales

--- Ejercicios_Manipulacion_DOM/JSproyectos/pagina-web/publico/js/main.js
+++ Ejercicios_Manipulacion_DOM/JSproyectos/pagina-web/publico/js/main.js
@@ -23,3 +23,17 @@ const textures = [
     "./images/img1.png",
 ].map(path => loader.load(path));
 
+/* Crear Materiales */
+const materials = textures.map(texture => new THREE.MeshBasicMaterial({
+    map: texture,
+    side: THREE.FrontSide,
+    transparent: true,
+    opacity: 1
+}));
+
+/* Material transparente para caras adicionales */
+const transparentMaterial = new THREE.MeshBasicMaterial({
+    color: 0x000000,
+    transparent: true,
+    opacity: 0
+});
