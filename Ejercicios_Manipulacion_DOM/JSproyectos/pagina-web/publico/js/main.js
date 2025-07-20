89e206add8d993268c54f32d9ed40bb02e68f036
BryanCachimuel <bryanloyo56@gmail.com>
2025-07-19 22:12:37 -0500

    Realizando el proceso de renderizado del fondo transparente

--- Ejercicios_Manipulacion_DOM/JSproyectos/pagina-web/publico/js/main.js
+++ Ejercicios_Manipulacion_DOM/JSproyectos/pagina-web/publico/js/main.js
@@ -1,4 +1,10 @@
-/* Configuración inicial de three.js */
+// Configuración inicial de Three.js
 const container = document.querySelector("#three-container");
 const scene = new THREE.Scene();
-const camera = new THREE.perspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
+
+// Renderizador con fondo transparente
+const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+renderer.setSize( container.clientWidth, container.clientHeight );
+renderer.setClearColor(0x000000, 0);
+container.appendChild(renderer.domElement);
