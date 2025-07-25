38e329945cece2ca9e4fb0e1668ba3a04cd320e7
BryanCachimuel <bryanloyo56@gmail.com>
2025-07-24 22:32:02 -0500

    Creando la función nueva tarea en donde se valida los valores que se ingresan por el formulario

--- Ejercicios_Manipulacion_DOM/JSproyectos/Check-Tareas/publico/js/main.js
+++ Ejercicios_Manipulacion_DOM/JSproyectos/Check-Tareas/publico/js/main.js
@@ -1,5 +1,17 @@
 const frmTask = document.querySelector("#frmTask");
+const inputTask = document.querySelector("#task");
 
 document.addEventListener("DOMContentLoaded", () => {
     frmTask.addEventListener("submit", newTask)
 });
+
+function newTask(e) {
+    e.preventDefault();
+    const task = inputTask.value.trim();
+
+    if(task.length === 0) {
+        showAlert('La tarea es requerida','error')
+        return;
+    }
+    console.log('Pasaste')  
+}
