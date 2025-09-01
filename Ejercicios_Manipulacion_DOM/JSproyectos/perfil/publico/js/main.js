1acf7f16668332eadd07b50103cd0f030db0be58
BryanCachimuel <bryanloyo56@gmail.com>
2025-08-31 20:57:52 -0500

    Creando la función loadUserProfile la cual integra los datos del profile en las respectivas etiquetas del maquetado y mediante la función addEventListener se carga esta función apenas se active el programa en el navegador

--- Ejercicios_Manipulacion_DOM/JSproyectos/perfil/publico/js/main.js
+++ Ejercicios_Manipulacion_DOM/JSproyectos/perfil/publico/js/main.js
@@ -1,6 +1,17 @@
-const userData = {
+const data = {
     name: "BLCL",
     description: "Programador y Desarrollador de software",
     email: "blcl@gmail.com",
     tel: "0985465987"
 }
+
+function loadUserProfile(data) {
+    document.getElementById("userName").textContent = data.name;
+    document.getElementById("userDescription").textContent = data.description;
+    document.getElementById("userEmail").textContent = data.Email;
+    document.getElementById("userTel").textContent = data.tel;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loadUserProfile();
+})
