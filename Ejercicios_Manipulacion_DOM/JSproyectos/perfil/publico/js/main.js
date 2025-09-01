0ac458d9ad99e546a21080eab09f5817a7ce29f1
BryanCachimuel <bryanloyo56@gmail.com>
2025-08-31 21:26:23 -0500

    Agregando un dato más al objeto y agrandolo dentro de la función loadUserProfile

--- Ejercicios_Manipulacion_DOM/JSproyectos/perfil/publico/js/main.js
+++ Ejercicios_Manipulacion_DOM/JSproyectos/perfil/publico/js/main.js
@@ -2,16 +2,18 @@ const data = {
     name: "BLCL",
     description: "Programador y Desarrollador de software",
     email: "blcl@gmail.com",
-    tel: "0985465987"
+    phone: "0985465987",
+    ubication: "Ecuador"
 }
 
 function loadUserProfile(data) {
     document.getElementById("userName").textContent = data.name;
     document.getElementById("userDescription").textContent = data.description;
-    document.getElementById("userEmail").textContent = data.Email;
-    document.getElementById("userTel").textContent = data.tel;
+    document.getElementById("userEmail").textContent = data.email;
+    document.getElementById("userTelefono").textContent = data.phone;
+    document.getElementById("userUbicacion").textContent = data.ubication;
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    loadUserProfile();
+    loadUserProfile(data);
 })
