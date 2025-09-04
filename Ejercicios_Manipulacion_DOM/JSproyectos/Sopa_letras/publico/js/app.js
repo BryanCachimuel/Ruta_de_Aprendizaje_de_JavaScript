f6ff4f648cd6567902cb12bbe0030a3d4c191eeb
BryanCachimuel <bryanloyo56@gmail.com>
2025-09-03 22:19:31 -0500

    Creando la función crearTablero para poder colocar las letras dentro de cada recuadro

--- Ejercicios_Manipulacion_DOM/JSproyectos/Sopa_letras/publico/js/app.js
+++ Ejercicios_Manipulacion_DOM/JSproyectos/Sopa_letras/publico/js/app.js
@@ -1,9 +1,9 @@
 /*Variables */
-let palabras = ["tortuga","gato","perro", "abeja", "perico", "tigre", "venado", "zorrillo", "ballena", "pantera", "oveja","cocodrilo","caballo","dinosaurio","escarabajo","rinoceronte","leopardo","jirafa","elefante","hipopotamo","unicornio","serpiente","gacela","mapache","chimpance","gorila","burro","zebra","leon","hormiga","manati","orangutan","mosquito","gallina","conejo","koala","tecolote"]; 
-let game = {c:15, r:15, letras:[], numPalabras:25, palabras:[], seleccionadas:[]};
-let b,a;
+let palabras = ["tortuga", "gato", "perro", "abeja", "perico", "tigre", "venado", "zorrillo", "ballena", "pantera", "oveja", "cocodrilo", "caballo", "dinosaurio", "escarabajo", "rinoceronte", "leopardo", "jirafa", "elefante", "hipopotamo", "unicornio", "serpiente", "gacela", "mapache", "chimpance", "gorila", "burro", "zebra", "leon", "hormiga", "manati", "orangutan", "mosquito", "gallina", "conejo", "koala", "tecolote"];
+let game = { c: 15, r: 15, letras: [], numPalabras: 25, palabras: [], seleccionadas: [] };
+let b, a;
 
-window.onload = function() {
+window.onload = function () {
     crearTablero();
     anexarPalabras();
     completarTablero();
@@ -13,19 +13,43 @@ window.onload = function() {
 
 /* funciones */
 function crearTablero() {
-    
+    var i = 0;
+    for (var r = 0; r < game.r; r++) {
+        for (var c = 0; c < game.c; c++) {
+            b = document.createElement("div");
+            b.innerText = "*";
+            a = document.createAttribute("id");
+            a.value = "celda-" + c + "-" + r;
+            b.setAttributeNode(a);
+            //
+            a = document.createAttribute("class");
+            a.value = "celda";
+            b.setAttributeNode(a);
+            //
+            b.addEventListener("click", (e) => {
+                var id = e.target.id;
+                console.log(id);
+            }, false);
+            //
+            document.getElementById("tablero").appendChild(b);
+            //
+            game.letras.push({ r: r, c: c, i: i, letra: "*" });
+            //
+            i++;
+        }
+    }
 }
 
 function anexarPalabras() {
-    
+
 }
 
 function completarTablero() {
-    
+
 }
 
 function listarPalabras() {
-    
+
 }
 
 function encabezado() {
