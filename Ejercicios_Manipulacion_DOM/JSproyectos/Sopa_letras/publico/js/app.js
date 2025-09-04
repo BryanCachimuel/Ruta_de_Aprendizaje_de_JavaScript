80ef91b6e1e94c51928d00987689dd503ec8be51
BryanCachimuel <bryanloyo56@gmail.com>
2025-09-03 21:48:31 -0500

    Creando las variables y la función principal que ejecutará lógica del proyecto

---
+++ Ejercicios_Manipulacion_DOM/JSproyectos/Sopa_letras/publico/js/app.js
@@ -0,0 +1,13 @@
+/*Variables */
+let palabras = ["tortuga","gato","perro", "abeja", "perico", "tigre", "venado", "zorrillo", "ballena", "pantera", "oveja","cocodrilo","caballo","dinosaurio","escarabajo","rinoceronte","leopardo","jirafa","elefante","hipopotamo","unicornio","serpiente","gacela","mapache","chimpance","gorila","burro","zebra","leon","hormiga","manati","orangutan","mosquito","gallina","conejo","koala","tecolote"]; 
+let game = {c:15, r:15, letras:[], numPalabras:25, palabras:[], seleccionadas:[]};
+let b,a;
+
+window.onload = function() {
+    crearTablero();
+    anexarPalabras();
+    completarTablero();
+    listarPalabras();
+    encabezado();
+}
+
