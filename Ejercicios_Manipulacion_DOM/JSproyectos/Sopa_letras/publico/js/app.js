f7b3b7df688a6e25068c9e493d35ae8181cd5e23
BryanCachimuel <bryanloyo56@gmail.com>
2025-09-05 21:04:47 -0500

    Realizando una validación para verificar el contador sobre una palabra que se este buscando en el eje de las x o de forma horizontal

--- Ejercicios_Manipulacion_DOM/JSproyectos/Sopa_letras/publico/js/app.js
+++ Ejercicios_Manipulacion_DOM/JSproyectos/Sopa_letras/publico/js/app.js
@@ -44,7 +44,7 @@ function anexarPalabras() {
     palabras = palabras.sort(() => Math.random() - 0.5);
     for (let i = 0; i < game.numPalabras; i++) {
         ok = anadirPalabra(palabras[i].toUpperCase());
-        if(ok) {
+        if (ok) {
             game.palabras.push({
                 palabra: palabras[i].toUpperCase(),
                 posicion: ok,
@@ -57,27 +57,39 @@ function anexarPalabras() {
 
 function anadirPalabra(palabra) {
     let palabra_array = palabra.split("");
-    palabra_array = (Math.random()>0.5)?palabra_array:palabra_array.reverse();
-    let pos = {col:0, row:0};
+    palabra_array = (Math.random() > 0.5) ? palabra_array : palabra_array.reverse();
+    let pos = { col: 0, row: 0 };
     pos.col = coordenada(palabra_array.length, game.c);
-    pos.row = Math.floor(Math.random()*game.r);
-    ok = verificarPalabraX(pos,palabra_array);
+    pos.row = Math.floor(Math.random() * game.r);
+    ok = verificarPalabraX(pos, palabra_array);
 }
 
 function verificarPalabraX(pos, palabra) {
-    let inicio = (pos.row*game.c)+pos.col;
+    let inicio = (pos.row * game.c) + pos.col;
     let contador = 0;
     let posicion = [];
     // realizar la busqueda
-    for(let i = 0; i < palabras.length; i++){
-        if(game.letras[inicio+i].letra=="*") {
+    for (let i = 0; i < palabra.length; i++) {
+        if (game.letras[inicio + i].letra == "*") {
             contador++;
         }
     }
+    if (contador == palabra.length) {
+        for (let i = 0; i < palabra.length; i++) {
+            if (game.letras[inicio + i].letra == "*") {
+               game.letras[inicio+i].letra = palabra[i];
+               let id = "celda-"+game.letras[inicio+i].c+"-"+game.letras[inicio+i].r;
+               document.getElementById(id).innerText = palabra[i];
+               posicion.push(inicio+i);
+            }
+        }
+        return ok;
+    }
+    return false;
 }
 
-function coordenada(lon,c) {
-    return Math.floor(Math.random()*(c-lon+1));
+function coordenada(lon, c) {
+    return Math.floor(Math.random() * (c - lon + 1));
 }
 
 function completarTablero() {
