4aa0699204c8712ff81f310f88f0af40f0088c8f
BryanCachimuel <bryanloyo56@gmail.com>
2025-09-06 22:45:57 -0500

    Dentro de la función verificarPalabras se realiza una validación para el tamaño de la letra seleccionada

--- Ejercicios_Manipulacion_DOM/JSproyectos/Sopa_letras/publico/js/app.js
+++ Ejercicios_Manipulacion_DOM/JSproyectos/Sopa_letras/publico/js/app.js
@@ -1,18 +1,17 @@
-/*Variables */
-let palabras = ["tortuga", "gato", "perro", "abeja", "perico", "tigre", "venado", "zorrillo", "ballena", "pantera", "oveja", "cocodrilo", "caballo", "dinosaurio", "escarabajo", "rinoceronte", "leopardo", "jirafa", "elefante", "hipopotamo", "unicornio", "serpiente", "gacela", "mapache", "chimpance", "gorila", "burro", "zebra", "leon", "hormiga", "manati", "orangutan", "mosquito", "gallina", "conejo", "koala", "tecolote"];
-let game = { c: 15, r: 15, letras: [], numPalabras: 25, palabras: [], seleccionadas: [] };
-let b, a;
+/* Variables */
+ var palabras = ["tortuga","gato","perro", "abeja", "perico", "tigre", "venado", "zorrillo", "ballena", "pantera", "oveja","cocodrilo","caballo","dinosaurio","escarabajo","rinoceronte","leopardo","jirafa","elefante","hipopotamo","unicornio","serpiente","gacela","mapache","chimpance","gorila","burro","zebra","leon","hormiga","manati","orangutan","mosquito","gallina","conejo","koala","tecolote"]; 
+ var game = {c:15, r:15, letras:[], numPalabras:25, palabras:[], seleccionadas:[]};
+ var b,a;
 
-window.onload = function () {
-    crearTablero();
-    anexarPalabras();
-    completarTablero();
-    listarPalabras();
-    encabezado();
-}
-
-/* funciones */
-function crearTablero() {
+ window.onload = function() {
+ 	crearTablero();
+ 	anexarPalabras();
+ 	completarTablero();
+ 	listarPalabras();
+ 	encabezado();
+ }
+ /* Funciones */
+ function crearTablero() {
  	var i = 0;
     for (var r = 0; r < game.r; r++) {
         for (var c = 0; c < game.c; c++) {
@@ -43,7 +42,7 @@ function crearTablero() {
             } else {
                document.getElementById(id).style.backgroundColor = "pink";
             }
-            verificaPalabras();
+            verificarPalabras();
            },false);
            //
            document.getElementById("tablero").appendChild(b);
@@ -55,151 +54,155 @@ function crearTablero() {
     }
     console.log(game.letras);
  }
+ function verificarPalabras() {
+   game.palabras.forEach((w,i)=>{
+      let verifica = 0;
+      game.seleccionadas.forEach((val,ii)=>{
+         if(w.posicion.includes(ii)){
+            verifica++;
+         }
+      });
+      if (verifica==w.palabra.length) {
+         document.getElementById(w.palabra).style.color="red";
+         document.getElementById(w.palabra).style.textDecoration = "line-through";
+         w.encontrada = true;
+      }
+   });
+   encabezado();
+ }
 
-
-function verificarPalabras() {
-    game.palabras.forEach((w,i) => {
-        let verifica = 0;
-        game.seleccionadas.forEach((val,ii)=>{
-            if(w.posicion.includes(ii)) {
-                verifica++;
-            }
-        }); 
-    });
-}
 
 function anexarPalabras() {
-    palabras = palabras.sort(() => Math.random() - 0.5);
-    for (let i = 0; i < game.numPalabras; i++) {
-        ok = anadirPalabra(palabras[i].toUpperCase());
-        if (ok) {
-            game.palabras.push({
-                palabra: palabras[i].toUpperCase(),
-                posicion: ok,
-                encontrada: false
-            });
-        }
+ 	palabras = palabras.sort(()=>Math.random() - 0.5);
+   //
+   for (var i = 0; i < game.numPalabras; i++) {
+      ok = anadirPalabra(palabras[i].toUpperCase());
+      if (ok) {
+         game.palabras.push({
+            palabra:palabras[i].toUpperCase(),
+            posicion: ok,
+            encontrada: false
+         });
+      }
+   }
+   console.log(game.palabras);
+ }
+
+ function anadirPalabra(palabra) {
+    var palabra_array = palabra.split("");
+    palabra_array = (Math.random()>0.5)?palabra_array:palabra_array.reverse();
+    var ok = false;
+    var tope = 300;
+    while(!ok && tope > 0){
+      tope--;
+      let pos = {col:0, row:0};
+      let dir = (Math.random()>0.5)?true:false;
+      if (dir && palabra_array.length<=game.c) {
+         //palabra horizontal
+         pos.col = coordenada(palabra_array.length, game.c);
+         pos.row = Math.floor(Math.random()*game.r);
+         ok = verificaPalabraX(pos,palabra_array);
+      } else if(!dir && palabra_array.length<=game.r){
+         //palabra vertical
+         pos.row = coordenada(palabra_array.length, game.r);
+         pos.col = Math.floor(Math.random()*game.c);
+         ok = verificaPalabraY(pos,palabra_array);
+      }
     }
-    console.log(game.palabras)
-}
+    return ok;
+ }
 
-function anadirPalabra(palabra) {
-    let palabra_array = palabra.split("");
-    palabra_array = (Math.random() > 0.5) ? palabra_array : palabra_array.reverse();
-    
-    let ok = false;
-    let tope = 300;
-
-    while(!ok && tope > 0) {
-        tope--;
-        let pos = { col: 0, row: 0 };
-        let dir = (Math.random() > 0.5) ? true : false;
-        if(dir && palabra_array.length <= game.c) {
-            // palabras en horizontal
-            pos.col = coordenada(palabra_array.length, game.c);
-            pos.row = Math.floor(Math.random() * game.r);
-            ok = verificarPalabraX(pos, palabra_array);
-        }else if(!dir && palabra_array.length <= game.r){
-            // palabras en vertical
-            pos.row = coordenada(palabra_array.length, game.r);
-            pos.col = Math.floor(Math.random() * game.c);
-            ok = verificarPalabraY(pos, palabra_array);
-        }
-    }  
-    return ok;  
-}
-
-function verificarPalabraY(pos, palabra) {
-    let inicio = (pos.row * game.c) + pos.col;
+ function verificaPalabraY(pos, palabra) {
+    let inicio = (pos.row*game.c)+pos.col;
     let contador = 0;
     let posicion = [];
-    // realizar la busqueda
-    for (let i = 0; i < palabra.length; i++) {
-        if (game.letras[inicio + (i*game.c)].letra == "*") {
-            contador++;
-        }
-    }
-    if (contador == palabra.length) {
-        for (let i = 0; i < palabra.length; i++) {
-            if (game.letras[inicio + (i*game.c)].letra == "*") {
-               game.letras[inicio+(i*game.c)].letra = palabra[i];
-               let id = "celda-"+game.letras[inicio+(i*game.c)].c+"-"+game.letras[inicio+(i*game.c)].r;
-               document.getElementById(id).innerText = palabra[i];
-               posicion.push(inicio+(i*game.c));
-            }
-        }
-        return posicion;
-    }
-    return false;
-}
-
-function verificarPalabraX(pos, palabra) {
-    let inicio = (pos.row * game.c) + pos.col;
-    let contador = 0;
-    let posicion = [];
-    // realizar la busqueda
-    for (let i = 0; i < palabra.length; i++) {
-        if (game.letras[inicio + i].letra == "*") {
-            contador++;
-        }
-    }
-    if (contador == palabra.length) {
-        for (let i = 0; i < palabra.length; i++) {
-            if (game.letras[inicio + i].letra == "*") {
-               game.letras[inicio+i].letra = palabra[i];
-               let id = "celda-"+game.letras[inicio+i].c+"-"+game.letras[inicio+i].r;
-               document.getElementById(id).innerText = palabra[i];
-               posicion.push(inicio+i);
-            }
-        }
-        return posicion;
-    }
-    return false;
-}
-
-function coordenada(lon, c) {
-    return Math.floor(Math.random() * (c - lon + 1));
-}
-
-function completarTablero() {
-    let id;
-    for (let i = 0; i < game.letras.length; i++) {
-       if(game.letras[i].letra == "*") {
-        game.letras[i].letra = letraAleatoria();
-        id = "celda-"+game.letras[i].c+"-"+game.letras[i].r;
-        document.getElementById(id).innerText =  game.letras[i].letra;
+    //busqueda
+    for (var i = 0; i < palabra.length; i++) {
+       if(game.letras[inicio+(i*game.c)].letra=="*"){
+         contador++;
        }
     }
-}
-
-function letraAleatoria() {
-    let ascci = Math.floor(Math.random()*26)+65;
-    return String.fromCharCode(ascci);
-}
-
-function listarPalabras() {
-    let d,a;
-    for (let i = 0; i < game.palabras.length; i++) {
-        d = document.createElement("div");
-        d.innerText = game.palabras[i].palabra;
-        a = document.createAttribute("id");
-        a.value = game.palabras[i].palabra;
-        d.setAttributeNode(a);
-        document.getElementById("palabras").appendChild(d); 
+    if (contador==palabra.length) {
+      for (var i = 0; i < palabra.length; i++) {
+         if (game.letras[inicio+(i*game.c)].letra=="*") {
+            game.letras[inicio+(i*game.c)].letra = palabra[i];
+            var id="celda-"+game.letras[inicio+(i*game.c)].c+"-"+game.letras[inicio+(i*game.c)].r;
+            document.getElementById(id).innerText = palabra[i];
+            posicion.push(inicio+(i*game.c));
+         }
+      }
+      return posicion;
     }
-}
+    return false;
+ }
 
-function encabezado() {
-    game.aciertos = 0;
-    game.palabras.forEach((w,i) => {
-        if(w.encontrada) {
-            game.aciertos++;
-        }
-    });
-    let faltan = game.palabras.length - game.aciertos;
-    if(faltan == 0) {
-        document.getElementById("mensaje").innerText = "Felicidades";
-    }else {
-        document.getElementById("mensaje").innerText = "Encuentre las siguientes "+ faltan + " palabras";
+ function verificaPalabraX(pos, palabra) {
+    let inicio = (pos.row*game.c)+pos.col;
+    let contador = 0;
+    let posicion = [];
+    //busqueda
+    for (var i = 0; i < palabra.length; i++) {
+       if(game.letras[inicio+i].letra=="*"){
+         contador++;
+       }
     }
-}
+    if (contador==palabra.length) {
+      for (var i = 0; i < palabra.length; i++) {
+         if (game.letras[inicio+i].letra=="*") {
+            game.letras[inicio+i].letra = palabra[i];
+            var id="celda-"+game.letras[inicio+i].c+"-"+game.letras[inicio+i].r;
+            document.getElementById(id).innerText = palabra[i];
+            posicion.push(inicio+i);
+         }
+      }
+      return posicion;
+    }
+    return false;
+ }
+ 
+ function coordenada(lon,c) {
+    return Math.floor(Math.random()*(c-lon+1));
+ }
+ 
+ function completarTablero() {
+   var id;
+ 	for (var i = 0; i < game.letras.length; i++) {
+     if(game.letras[i].letra == "*"){
+      game.letras[i].letra=letraAleatoria();
+      id = "celda-"+game.letras[i].c+"-"+game.letras[i].r;
+      document.getElementById(id).innerText = game.letras[i].letra;
+     }
+   }
+ }
+ function letraAleatoria() {
+    let ascii = Math.floor(Math.random()*26)+65;
+    return String.fromCharCode(ascii);
+ }
+
+ function listarPalabras() {
+   var d,a;
+   for (var i = 0; i < game.palabras.length; i++) {
+     d = document.createElement("div");
+     d.innerText = game.palabras[i].palabra;
+     //
+     a = document.createAttribute("id");
+     a.value = game.palabras[i].palabra;
+     d.setAttributeNode(a);
+     //
+     document.getElementById("palabras").appendChild(d);
+   }
+ }
+ function encabezado() {
+ 	game.aciertos = 0;
+   game.palabras.forEach((w,i)=>{
+      if (w.encontrada) {
+         game.aciertos++;
+      }
+   });
+   var faltan = game.palabras.length - game.aciertos;
+   if (faltan==0) {
+      document.getElementById("mensaje").innerText = "Felicidades";
+   } else {
+      document.getElementById("mensaje").innerText = "Encuentra las siguientes "+faltan+" palabras";
+   }
+ }
