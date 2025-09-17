188ebb6c007fb97a3c04c0d6c420fcb5b90e69c6
BryanCachimuel <bryanloyo56@gmail.com>
2025-09-16 21:14:57 -0500

    Realizando un ciclo for para analizar los datos que esten en cada tr y poder realizar la filtración de las palabras que se ingresen mediante el input de busqueda

--- Ejercicios_Manipulacion_DOM/tabla_dinamica/fitro_tabla/tabla.js
+++ Ejercicios_Manipulacion_DOM/tabla_dinamica/fitro_tabla/tabla.js
@@ -2,10 +2,22 @@ function filtro() {
     let entrada, tabla, tr, td, i, txtValor;
     
     entrada = document.getElementById("entrada");
-    filtro = entrada.value.toUpperCase();
+    filtro_txt = entrada.value.toUpperCase();
 
     tabla = document.getElementById("misLibros");
 
     // obtenemos todas las etiquetas de tipo tr
     tr = tabla.getElementsByTagName("tr");
+
+    for (let i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if(td) {
+            txtValor = td.textContent || td.innerText;
+            if(txtValor.toUpperCase().indexOf(filtro_txt) > -1) {
+                tr[i].style.display = "";
+            }else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
 }
