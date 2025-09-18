0b58b41025270e7eaeb8a52343a41f592acf5f67
BryanCachimuel <bryanloyo56@gmail.com>
2025-09-18 16:53:23 -0500

    Creando las funciones foto y muestraFoto

--- Ejercicios_Manipulacion_DOM/tabla_dinamica/slideshow/main.js
+++ Ejercicios_Manipulacion_DOM/tabla_dinamica/slideshow/main.js
@@ -7,3 +7,15 @@ window.onload = function() {
 function mueve(n) {
     muestraFoto(index += n);
 }
+
+function foto(n) {
+   muestraFoto(index = n); 
+}
+
+function muestraFoto(n) {
+    let i;
+    let fotos = document.getElementsByClassName("foto");
+    let bolitas = document.getElementsByClassName("bolita");
+    if(n > fotos.length) index = 1;
+    if(n < 1) index = fotos.length;
+}
